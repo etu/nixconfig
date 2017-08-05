0ae505fe4c471aaa0db68abc7f71da7778193df8
Elis Axelsson <elis.axelsson@gmail.com>
2017-08-05 22:54:20 +0200

    Don't ignore for hactar since it doesn't even run nixos yet

--- home-etu-nixpkgs/dotfiles/fish/functions/fish_right_prompt.fish
+++ home-etu-nixpkgs/dotfiles/fish/functions/fish_right_prompt.fish
@@ -1,12 +1,10 @@
 function fish_right_prompt --description 'Display right prompt'
     set PROMPT (set_color -b 585858)(set_color bbbbbb)" "(date +%H:%M:%S)" "
 
-    if test (hostname) != "hactar"
-        set git_prompt (__fish_git_prompt ' %s ')
+    set git_prompt (__fish_git_prompt ' %s ')
 
-        if test $status = 0
-            set PROMPT (set_color -b 444444)(set_color bbbbbb)$git_prompt$PROMPT
-        end
+    if test $status = 0
+        set PROMPT (set_color -b 444444)(set_color bbbbbb)$git_prompt$PROMPT
     end
 
     builtin echo -ns $PROMPT(set_color normal)
