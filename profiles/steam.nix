3047ba4d21d45f016d2e187cd15fb3debc0c63d5
Elis Axelsson <elis.axelsson@gmail.com>
2017-09-01 18:48:33 +0200

    Enable support for steam controller and 32bit pulseaudio when using steam

--- profiles/steam.nix
+++ profiles/steam.nix
@@ -7,6 +7,15 @@
     driSupport32Bit = true;
   };
 
+  # Support 32bit pulseaudio
+  hardware.pulseaudio.support32Bit = true;
+
+  # Enable udev rules for steam controller
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", MODE="0666"
+    KERNEL=="uinput", MODE="0660", GROUP="users", OPTIONS+="static_node=uinput"
+  '';
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
