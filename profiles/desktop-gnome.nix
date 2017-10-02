1930510f072671686a896a03bfe1daed104f257d
Elis Hirwing <elis@hirwing.se>
2017-10-02 20:18:41 +0200

    Add all the ugly hacks to gnome module

--- profiles/desktop-gnome.nix
+++ profiles/desktop-gnome.nix
@@ -10,6 +10,28 @@
     gnomeExtensions.topicons-plus
   ];
 
+  # Ugly hack to get gdm and gnome to understand keyboard settings
+  # https://github.com/NixOS/nixpkgs/issues/14318#issuecomment-309250231
+  environment.etc."X11/xorg.conf.d/00-keyboard.conf".text = ''
+    Section "InputClass"
+      Identifier "Keyboard catchall"
+      MatchIsKeyboard "on"
+      Option "XkbRules" "base"
+      Option "XkbModel" "pc105"
+      Option "XkbLayout" "se"
+      Option "XkbOptions" "eurosign:e,ctrl:nocaps,numpad:mac,kpdl:dot"
+      Option "XkbVariant" "dvorak"
+    EndSection
+  '';
+
+  # Another ugly hack to get gdm and gnome to understand keyboard settings
+  # https://github.com/NixOS/nixpkgs/issues/14318#issuecomment-330193990
+  services.xserver.desktopManager.gnome3.extraGSettingsOverrides = ''
+    [org.gnome.desktop.input-sources]
+    sources=[('xkb', 'se+dvorak')]
+    xkb-options=['eurosign:e', 'ctrl:nocaps', 'numpad:mac', 'kpdl:dot']
+  '';
+
   # Enable the Plasma Desktop Environment.
   services.xserver.desktopManager.gnome3.enable = true;
 
