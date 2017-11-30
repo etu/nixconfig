b523001b7a98775a6f2bc83a946766800e5c7f10
Elis Hirwing <elis@hirwing.se>
2017-11-30 11:55:39 +0100

    Fix keyboard layout issues. This fixes https://github.com/NixOS/nixpkgs/issues/32195

--- profiles/desktop-gnome.nix
+++ profiles/desktop-gnome.nix
@@ -12,20 +12,6 @@
   ];
 
   # Ugly hack to get gdm and gnome to understand keyboard settings
-  # https://github.com/NixOS/nixpkgs/issues/14318#issuecomment-309250231
-  environment.etc."X11/xorg.conf.d/00-keyboard.conf".text = ''
-    Section "InputClass"
-      Identifier "Keyboard catchall"
-      MatchIsKeyboard "on"
-      Option "XkbRules" "base"
-      Option "XkbModel" "pc105"
-      Option "XkbLayout" "se"
-      Option "XkbOptions" "eurosign:e,ctrl:nocaps,numpad:mac,kpdl:dot"
-      Option "XkbVariant" "dvorak"
-    EndSection
-  '';
-
-  # Another ugly hack to get gdm and gnome to understand keyboard settings
   # https://github.com/NixOS/nixpkgs/issues/14318#issuecomment-330193990
   services.xserver.desktopManager.gnome3.extraGSettingsOverrides = ''
     [org.gnome.desktop.input-sources]
