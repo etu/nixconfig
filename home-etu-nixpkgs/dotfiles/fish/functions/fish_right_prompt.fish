4abb7172d8d4622989bd14274f2a8eb7664b1d76
Elis Hirwing <elis@hirwing.se>
2018-01-01 17:56:24 +0100

    Add snowflake if in nix shell

--- home-etu-nixpkgs/dotfiles/fish/functions/fish_right_prompt.fish
+++ home-etu-nixpkgs/dotfiles/fish/functions/fish_right_prompt.fish
@@ -7,5 +7,9 @@ function fish_right_prompt --description 'Display right prompt'
         set PROMPT (set_color -b 444444)(set_color bbbbbb)$git_prompt$PROMPT
     end
 
+    if test "$IN_NIX_SHELL" = 1
+        set PROMPT (set_color -b 0087af)(set_color faf5e3)" ❄ ️"$PROMPT
+    end
+
     builtin echo -ns $PROMPT(set_color normal)
 end
