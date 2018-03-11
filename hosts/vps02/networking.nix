04a4cd40748551b8251592068ba0a467ad693fab
Elis Hirwing <elis@hirwing.se>
2018-03-11 13:27:55 +0100

    Fix warning about networking settings

--- hosts/vps02/networking.nix
+++ hosts/vps02/networking.nix
@@ -13,11 +13,11 @@
     defaultGateway6 = "";
     interfaces = {
       eth0 = {
-        ip4 = [
+        ipv4.addresses = [
           { address="188.226.173.51"; prefixLength=24; }
           { address="10.14.0.5"; prefixLength=16; }
         ];
-        ip6 = [
+        ipv6.addresses = [
           { address="2a03:b0c0:0:1010::13d5:1"; prefixLength=64; }
           { address="fe80::8097:22ff:fea2:2958"; prefixLength=64; }
         ];
