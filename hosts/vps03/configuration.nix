e47e9612f8ea24658574ead8a7aa2bdebd707064
Elis Hirwing <elis@hirwing.se>
2018-03-17 13:21:09 +0100

    vps03: move (www.)elis.nu from vps01

--- hosts/vps03/configuration.nix
+++ hosts/vps03/configuration.nix
@@ -10,4 +10,28 @@
   # Set up bootloader
   boot.loader.grub.device = "/dev/vda";
   boot.cleanTmpDir = true;
+
+  # Caddy
+  services.caddy.enable = true;
+  services.caddy.agree = true;
+  services.caddy.email = "elis@hirwing.se";
+  services.caddy.config = ''
+    elis.nu, www.elis.nu {
+      tls {
+        protocols tls1.2
+        key_type p384
+      }
+
+      header / {
+        Strict-Transport-Security max-age=31536000
+      }
+
+      proxy / https://etu.github.io/ {
+        header_upstream Host {host}
+      }
+    }
+  '';
+
+  # Firewall
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
