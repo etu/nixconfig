fef46fc0aaadf806f86654bcdf3e0dc1cb37bd7d
Elis Hirwing <elis@hirwing.se>
2018-03-27 23:44:36 +0200

    vps02: Set up coturn server

--- profiles/matrix.nix
+++ profiles/matrix.nix
@@ -1,10 +1,19 @@
 { pkgs, ... }:
 
-{
+let
+  coturn-ports = [ 3478 3479 5349 5350 ];
+  http-ports = [ 80 443 ];
+  coturn-shared-secret = builtins.readFile /var/lib/coturn-shared-secret.key;
+
+in {
+  users.groups.matrix-tls = {
+    members = [ "matrix-synapse" "turnserver" ];
+  };
+
   # Cert renewal
   security.acme.certs = {
     "matrix.failar.nu" = {
-      group = "matrix-synapse";
+      group = "matrix-tls";
       allowKeysForGroup = true;
       postRun = "systemctl reload nginx.service; systemctl restart matrix-synapse.service";
     };
@@ -22,8 +31,6 @@
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
-
   # Synapse
   services.matrix-synapse.enable = true;
   services.matrix-synapse.web_client = true;
@@ -31,6 +38,11 @@
   services.matrix-synapse.database_type = "psycopg2";
   services.matrix-synapse.public_baseurl = "https://matrix.failar.nu/";
   # services.matrix-synapse.registration_shared_secret = "shared-secret-used-for-commandline-creation-of-users";
+  services.matrix-synapse.turn_shared_secret = coturn-shared-secret;
+  services.matrix-synapse.turn_uris = [
+    "turn:matrix.failar.nu:3478?transport=udp"
+    "turn:matrix.failar.nu:3478?transport=tcp"
+  ];
 
   services.matrix-synapse.tls_certificate_path = "/var/lib/acme/matrix.failar.nu/fullchain.pem";
   services.matrix-synapse.tls_private_key_path = "/var/lib/acme/matrix.failar.nu/key.pem";
@@ -56,4 +68,17 @@
   services.postgresql.enable = true;
   services.postgresql.package = pkgs.postgresql100;
   services.postgresql.dataDir = "/var/lib/postgresql/10.0";
+
+  # Coturn
+  services.coturn.enable = true;
+  services.coturn.lt-cred-mech = true;
+  services.coturn.use-auth-secret = true;
+  services.coturn.static-auth-secret = coturn-shared-secret;
+  services.coturn.realm = "failar.nu";
+  services.coturn.cert = "/var/lib/acme/matrix.failar.nu/fullchain.pem";
+  services.coturn.pkey = "/var/lib/acme/matrix.failar.nu/key.pem";
+
+  # Firewall things
+  networking.firewall.allowedTCPPorts = http-ports ++ coturn-ports;
+  networking.firewall.allowedUDPPorts = coturn-ports;
 }
