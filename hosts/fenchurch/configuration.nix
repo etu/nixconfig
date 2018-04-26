c82346000a9b5114b312f04aac350050be6548da
Elis Hirwing <elis@hirwing.se>
2018-04-26 21:59:48 +0200

    hosts.fenchurch: upgrade kernel

--- hosts/fenchurch/configuration.nix
+++ hosts/fenchurch/configuration.nix
@@ -19,7 +19,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_4_14;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Hardware settings
   hardware.cpu.intel.updateMicrocode = true;
