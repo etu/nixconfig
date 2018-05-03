af41e1e8050a3cd55c189262c1866b925616b55d
Elis Hirwing <elis@hirwing.se>
2018-05-03 16:53:14 +0200

    hosts.ford: Disable fwupd

--- hosts/ford/configuration.nix
+++ hosts/ford/configuration.nix
@@ -34,9 +34,6 @@
   hardware.pulseaudio.enable = true;
   hardware.pulseaudio.package = pkgs.pulseaudioFull;
 
-  # Enable the fw update manager
-  services.fwupd.enable = true;
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
