2c4e5b1d8ba4f7011cfda891ef261e8c72fce9a4
Elis Hirwing <elis@hirwing.se>
2018-05-16 08:33:50 +0200

    home.fish.prompt: add quotes to make it not throw errors when variable is missing

--- home-etu-nixpkgs/dotfiles/fish/functions/fish_right_prompt.fish
+++ home-etu-nixpkgs/dotfiles/fish/functions/fish_right_prompt.fish
@@ -7,8 +7,8 @@ function fish_right_prompt --description 'Display right prompt'
         set PROMPT (set_color -b 444444)(set_color bbbbbb)$git_prompt$PROMPT
     end
 
-    if test -n $IN_NIX_SHELL
-        if test $IN_NIX_SHELL = "pure"
+    if test -n "$IN_NIX_SHELL"
+        if test "$IN_NIX_SHELL" = "pure"
             set PROMPT (set_color -b 0087af)(set_color faf5e3)" ❄ ️"$PROMPT
         else
             set PROMPT (set_color -b 897e01)(set_color faf5e3)" ❄ ️"$PROMPT
