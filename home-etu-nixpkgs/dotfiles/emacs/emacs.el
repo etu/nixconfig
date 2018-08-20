7fe42f5d9a0be1d9f982a27090f4742a54333f55
Elis Hirwing <elis@hirwing.se>
2018-08-20 16:32:16 +0200

    emacs-config: Tune garbage collector and stop byte compiling the config

--- home-etu-nixpkgs/dotfiles/emacs/emacs.el
+++ home-etu-nixpkgs/dotfiles/emacs/emacs.el
@@ -16,17 +16,21 @@
 (defvar user-emacs-cache-directory
   (concat (getenv "HOME") "/.cache/emacs"))
 
+;; Increase the threshold to reduce the amount of garbage collections made
+;; during startups.
+(let ((gc-cons-threshold (* 50 1000 1000))
+      (gc-cons-percentage 0.6))
 
-;; Init package manager
-(package-initialize)
+  ;; Init package manager
+  (package-initialize)
 
 
-;; Avoid creation of ~/.emacs.d/
-(setq-default user-emacs-directory user-emacs-data-directory)
+  ;; Avoid creation of ~/.emacs.d/
+  (setq-default user-emacs-directory user-emacs-data-directory)
 
 
-;; Load config
-(org-babel-load-file (concat user-emacs-config-directory "/config.org") t)
+  ;; Load config
+  (org-babel-load-file (concat user-emacs-config-directory "/config.org")))
 
 
 ;;; emacs.el ends here
