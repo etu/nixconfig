b45c14910072bec56091b1070132411f06c5a7b4
Elis Hirwing <elis@hirwing.se>
2018-09-16 19:55:22 +0200

    hosts.fenchurch: Upgrade system and fix nvidia/kernel compatibility

--- hosts/fenchurch/configuration.nix
+++ hosts/fenchurch/configuration.nix
@@ -26,7 +26,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_4_17;
+  boot.kernelPackages = pkgs.linuxPackages_4_18;
 
   # Hardware settings
   hardware.cpu.intel.updateMicrocode = true;
