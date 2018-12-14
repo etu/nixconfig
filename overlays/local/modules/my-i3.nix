153b78304c3c64506b115c1233401ac8aac8465d
Elis Hirwing <elis@hirwing.se>
2018-12-14 20:58:12 +0100

    modules/i3: Set up Evolution and add some comments

--- overlays/local/modules/my-i3.nix
+++ overlays/local/modules/my-i3.nix
@@ -19,25 +19,38 @@ in {
   };
 
   config = mkIf cfg.enable {
+    # Libinput
     services.xserver.libinput.enable = true;
 
+    # Loginmanager
     services.xserver.displayManager.lightdm.enable = true;
     services.xserver.displayManager.lightdm.autoLogin.enable = true;
     services.xserver.displayManager.lightdm.autoLogin.user = "etu";
 
+    # Needed for autologin
     services.xserver.desktopManager.default = "none";
     services.xserver.windowManager.default = "i3";
 
+    # Set up i3
     services.xserver.windowManager.i3.enable = true;
     services.xserver.windowManager.i3.extraPackages = with pkgs; [
       dmenu
       i3status
       i3lock
-      rxvt_unicode
       pavucontrol
       gnome3.networkmanagerapplet
     ];
 
+    # Set up services needed for gnome stuff for evolution
+    services.gnome3.evolution-data-server.enable = true;
+    services.gnome3.gnome-keyring.enable = true;
+
+    # Install aditional packages
+    environment.systemPackages = with pkgs; [
+      gnome3.evolution
+    ];
+
+    # Configure TERMINAL for i3-sensible-terminal
     environment.variables.TERMINAL = "stupidterm";
   };
 }
