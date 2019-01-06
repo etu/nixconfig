2e2d25890a0a98f6d3013b51304a37008032dca7
Elis Hirwing <elis@hirwing.se>
2019-01-06 10:15:06 +0100

    hosts/phouchg: Switch to exwm

--- hosts/phouchg/configuration.nix
+++ hosts/phouchg/configuration.nix
@@ -65,8 +65,8 @@ in {
   # Enable emacs deamon stuff
   my.emacs.enable = true;
 
-  # Enable my i3 desktop settings
-  my.i3.enable = true;
+  # Enable my exwm desktop settings
+  my.exwm.enable = true;
 
   # Define a user account.
   my.user.enable = true;
