b0c8debd39533b4d69218575a5785ea376f31d44
Elis Hirwing <elis@hirwing.se>
2019-03-19 07:26:44 +0100

    modules/vbox: Restore vagrant override made in dc09fc1

--- overlays/local/modules/my-vbox.nix
+++ overlays/local/modules/my-vbox.nix
@@ -10,9 +10,7 @@ in {
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      (vagrant.override (oldAttrs: rec {
-        withLibvirt = false;
-      }))
+      vagrant
     ];
 
     # Enable virtualbox.
