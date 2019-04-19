91a3f9691c36d0ff482ce5f5150165657c48e990
Elis Hirwing <elis@hirwing.se>
2019-04-19 11:39:14 +0200

    hosts/ford-x250: Fork the config for a new laptop

---
+++ hosts/ford-x250/configuration.nix
@@ -0,0 +1,107 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    # Import local modules & overlays
+    ../../overlays/local/default.nix
+
+    # Import the home-manager module
+    <home-manager/nixos>
+  ];
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "19.03";
+
+  # Use local nixpkgs checkout
+  nix.nixPath = [
+    "nixpkgs=/etc/nixos/nixpkgs"
+    "home-manager=/nix/var/nix/profiles/per-user/root/channels/home-manager/"
+    "nixos-config=/etc/nixos/configuration.nix"
+  ];
+
+  networking.hostName = "ford-x250";
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.extraModulePackages = [
+    pkgs.linuxPackages_latest.acpi_call
+  ];
+
+  # Fix touchpad scrolling after suspend.
+  boot.kernelParams = [ "psmouse.synaptics_intertouch=0" ];
+
+  boot.cleanTmpDir = true;
+
+  # Hardware settings
+  services.xserver.videoDrivers = ["intel" "modesetting"];
+  hardware.trackpoint.enable = true;
+  hardware.cpu.intel.updateMicrocode = true;
+
+  # Enable TLP
+  services.tlp.enable = true;
+
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
+  # Enable pulse with all the modules
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.package = pkgs.pulseaudioFull;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  services.printing.drivers = [
+    pkgs.postscript-lexmark
+  ];
+
+  # Enable SANE to handle scanners
+  hardware.sane.enable = true;
+
+  # Disable root login for ssh
+  services.openssh.permitRootLogin = "no";
+
+  # Enable common cli settings for my systems
+  my.common-cli.enable = true;
+
+  # Enable aspell and hunspell with dictionaries.
+  my.spell.enable = true;
+
+  # Enable gpg related stuff
+  my.gpg-utils.enable = true;
+
+  # Enable common graphical stuff
+  my.common-graphical.enable = true;
+
+  # Enable emacs deamon stuff
+  my.emacs.enable = true;
+
+  # Enable my exwm desktop settings
+  my.exwm.enable = true;
+
+  # Enable dmrconfig to configure my hamradio.
+  programs.dmrconfig.enable = true;
+
+  # Define a user account.
+  my.user.enable = true;
+  my.user.extraGroups = [
+    "scanner"
+    "docker"
+    "libvirtd"
+  ];
+
+  # Home-manager as nix module
+  home-manager.users.etu = import ../../home-etu-nixpkgs/home.nix;
+
+  # Enable kvm
+  virtualisation.libvirtd.enable = true;
+
+  # Enable docker
+  virtualisation.docker.enable = true;
+}
