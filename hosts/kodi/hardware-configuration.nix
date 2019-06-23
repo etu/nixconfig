2713082a411851a1b009a4aed8a5ea996c389214
Elis Hirwing <elis@hirwing.se>
2019-06-23 09:46:19 +0200

    hosts/kodi: Enable the magic mirror docker image

--- hosts/kodi/hardware-configuration.nix
+++ hosts/kodi/hardware-configuration.nix
@@ -15,7 +15,7 @@
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
-    options = [ "defaults" "size=1G" "mode=755" ];
+    options = [ "defaults" "size=2G" "mode=755" ];
   };
 
   fileSystems."/boot" = {
