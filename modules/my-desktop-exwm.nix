9c0449a49a45e74df66cbd121bb3942a5a0ebbc1
Elis Hirwing <elis@hirwing.se>
2019-08-14 00:16:52 +0200

    modules/exwm: Add script to notify on low battery level

--- modules/my-desktop-exwm.nix
+++ modules/my-desktop-exwm.nix
@@ -17,6 +17,33 @@ let
     substituteAll ${myExwmInitPlain} $out
   '');
 
+  # Wrap up i3-battery-popup script to send notifications on low battery
+  i3BatteryPopup = pkgs.stdenv.mkDerivation rec {
+    pname = "i3-battery-popup";
+    version = "unstable-2019-06-07";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "rjekker";
+      repo = "i3-battery-popup";
+      rev = "d894a102a1ff95019fc59d0a19c89687d502cd1a";
+      sha256 = "12ym2mlv9jk1n8cigjxbqpffch4m4s5vj5rfmf9w2rl5l6g9b8vf";
+    };
+
+    nativeBuildInputs = with pkgs; [ makeWrapper ];
+    buildInputs = with pkgs; [ coreutils gawk gnugrep i3 libnotify pulseaudio tk ];
+
+    installPhase = ''
+      install -D i3-battery-popup $out/bin/i3-battery-popup
+
+      substituteInPlace $out/bin/i3-battery-popup \
+        --replace /usr/share/icons/gnome ${pkgs.gnome3.defaultIconTheme}/share/icons/Adwaita \
+        --replace /usr/share/icons/elementary-xfce ${pkgs.elementary-xfce-icon-theme}/share/icons/elementary-xfce
+
+      wrapProgram $out/bin/i3-battery-popup \
+        --prefix PATH ':' ${pkgs.stdenv.lib.makeBinPath buildInputs}
+    '';
+  };
+
 in {
   options.my.desktop-exwm.enable = mkEnableOption "Enables exwm and auto login for my user";
 
@@ -38,6 +65,17 @@ in {
     # Needed for autologin
     services.xserver.desktopManager.default = "none";
     services.xserver.windowManager.default = "exwm";
+
+    # Start i3-battery-popup for my user
+    systemd.user.services.i3-battery-popup = {
+      description = "i3-battery-popup";
+      wantedBy = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      serviceConfig = {
+        ExecStart = "${i3BatteryPopup}/bin/i3-battery-popup -n";
+        Restart = "always";
+      };
+    };
 
     # Enable i3lock on suspend
     systemd.services.i3lock = {
