a80f3104c6c165a828bd89fdee3d0c232b20b023
Elis Hirwing <elis@hirwing.se>
2019-09-19 22:12:16 +0200

    treewide: Move rest of the public keys to the central place

--- hosts/vps04/configuration.nix
+++ hosts/vps04/configuration.nix
@@ -92,13 +92,7 @@ in {
       isNormalUser = true;
       home = "/home/concate";
       uid = 1001;
-      openssh.authorizedKeys.keys = [
-        "no-agent-forwarding,no-X11-forwarding,permitopen=\"localhost:8002\",command=\"echo 'This account can only be used for weechat relays'\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQqvuRrzAcmp2neNQi/gDEQy6pe9EF2+pDWOTjmdC3+JDSOnZMHm9PX5e7YLZNk46YMSvia0XALQ16/Ao1xqsgyC1YRa8lWOxFn/8rmMIStSO2HKzX6LNJafqlFm5FAJO7znYYXImB0JHOOF43P8RzxQZ+X/MCt0SKYxsM8JeBKFzyDqlzxA7pqTH81DMENL9xT1rlaElg86JmTJWcQWriJVCCf6wi5bSlArCghQ1gU8oOhDGGB9OhCMj9KeumrbVzyZO5e03anX066bQnfTve8Tg1pxCXPse1kzyqkHrD4mVTA2jmVkiPT72WkZOiegOEi3CA1DfKUIfiFw5i6KNj conacte@android-2048-2018-02-21"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE/CqVKQd8Bk/e99BQ6floXFMhNZ3d3anPIKl9+RyXzO concate@gaara.2018-07-13"
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSpRskzDJwIYz6uVK9Yvl0B2I8zlnVC4KOOu/Jr+bhTW9aXvbrhVd6y1u+vicJ78j25MvzH3CXQY0D59CEnYYD1QlHIMN3fRICcrcbTNIhGaeVsTBrzvgeeMLluzTCe0fyve7lY2P1drn55lA+Mg6Au8oSho2904nOjiCcB/glnQ38Mu4nvyjm/dVBw83Dq2sOl+gyCtsTlIv6Gc7r7AafGComXDVrVbicPd2ig654rtz7L1LoCye298CJLXUJhJolfxhP+F4GDAqdjkj4o3MwlYrv6HNu6LYzcWKlagkL5m5LhSYC0uPytXWHYRO/mIvwQmFOlAI1sQ/q/ynHtFnp concate@hei"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH4+d3/gP+eiOTap7+u5tfX94ijF65kA0ilMryZYBuHD concate@hactar.2016-10-15"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMO3i0Y8/MGyf6Lz3AF3LWrt/JrpLt1YAiK7bYGzJJs4 concate@rocklee20190407"
-      ];
+      openssh.authorizedKeys.keys = keys.concate;
     };
 
     talyz = {
@@ -106,13 +100,7 @@ in {
       shell = pkgs.fish;
       home = "/home/talyz";
       uid = 1002;
-      openssh.authorizedKeys.keys = [
-        "no-agent-forwarding,no-X11-forwarding,permitopen=\"localhost:8003\",command=\"echo 'This account can only be used for weechat relays'\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDZKYLFI5eyv4/aZR8YUBLQagMoH4gyltcXiom/ZA9XpgtKy3fOxmX0NzETxVBdbc/mhU6s9zKNBXWDl5OS17a+GNPStPpUjMGB/ehTK+FkcNOHsqZ60QLHzejWbMfOFiZ9idPsVpvWl8o95YzUAZc61NlVMR1r9fys1CZtFk6ZfYuGLofnG3BafCeMY6BkWZ7Hi/+AoWxe57CIJzwmy3aBpQ7NxpS5vZl8/DGThLuRK4Ew6w1/TmRTTJjTr0USKPSI1V3XYZDkN3BZ4dRyIerpMEPhYfsLum1Qj+Oc60EziERsSadk4UnJt3ye4VjTcv+1za/CeDz6zAjgpEbfEBAN8xpNsfdjeFedw32YGRFhWpC2xg/yho7c2n8w5IOWKObggKZYxLBatzKpg0eYO5B/jBNnM6HIW/wfiKunnYrJMJHwzFSYZCyIRlZKb6yWRcW1+RlvlmlE95bAxPWmrFbEtOoPVESAR6lfTleKPMgjTGJ3KvYzp1k8jplJcQpbPmMiejkheh3tcKWn/Na3EfV3pUWGHtU6CQauHYdDBAfHnAD9baAHEpy3HGYz+0cgH4kKSas4U8ICaFeOE3OEv90caHyGHWaL5ynvq4ywUAlJUWYLIseHq4+aSxKj8aK/Nr6zfDUNAAOEaveT/YUeFk5LxaEXowkW/98nUwZ//G7vKw== talyz@android-2017-03-03"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPqiNApj9AcGJ4JfsDTdhSlfwmuDzKJmVrOGJibYKOo5 root@raine-2017-02-18"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC0Ny2gtIq74qiHph/5ZUyNkdVTTXj2lnuDRHpgR3fPi talyz@flora-2017-01-10"
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC5RDs62krOSjnSS26y9nPpP0jpIHSO9uQ1tMVd4kAuQlAp0Q1Nfq9YwImXccdDl/wEycreT17wRLO9Oq8w2Nlnp4d7Xe+5acadcCSNx0rd8773357/n/QK2GyPa9ysDP3HnNLK6QhBf4GVdtCoHflk1liONzSdhiCGPHvpYFowG67cZTzb4/YPvryS//478OEa2JutTrP+N7fjH+SHuzndiWSNvWXTlqXWIufXKPqFzaKueLuABUSLe/Sj7ZNJhcMrBPUKmtKCQSbrAmdYvk5WzX6FacMlUWACNDceXH31u7OVLXDjQYU2jlc5E3s60erNlHlLtRGSd+wqCQv21ttlhCi9EVqdByEGv9ZGjt9lq4GS0ZHZCHTts17hjYSYql2QnYyx5ypSLOyqSjA5n34V3QUcVCCBk8QCt4wrWKqmU79TSOK0ihCaZiYlicQcKUgGdU/ouGgWp9+fUu4BqNKG8EIEmgIgbvJoe0QohDTJ9EyAkxMUz3L6KhFpVv3Zd470pChYgCMsjowrKJAWzq75UszCelRTFIylmE7LmAo9OSU8MHSD3ihf07NNXa0rw1t15pMOFjCwQWj2ea3tBeUUVLb4BTdiH4P//5lsHckeA6d1xtniP7HQXJ3Vft6IL+igZ/npL4yK412yMXm05ufzfG8dsuMW4k9eVQbXBQeeOQ== talyz@natani-2018-02-12"
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQClEBWe28hb+GRtpa0MdKh/2UKPIANBEeDuXlcfHgS4Mi4gTNKMWjclXhBzEYfRLpYEZuBjcciRTKNKUjMi4aSmc3g6/FoueaDTmHhoQWCEFwrR7m1ZwplHYZuad2Dm6kOMXyi3VIw1y4u3K832LsTrrgBeDT+C23qVfjvmeytD7tWnoEqgDKnrdxZYqiNdu43HA5V7r7jXCMVby2/39iqa+AxKBxt/v1gz7rar3jr/6EfE55oJpQfj8wFGLq88IK915eTTEVYSZLUxZkfOaZGMjkyMiXNTLWtJ/MfBQ0SagDwwuZKf/+C/O1vO6scz6Uc9wBUPPbUnhUkGzO/kWduXiXLQfEwYrFVAd2HyrErwnuJs/0HSWYm/c6o4O1xaDaqj+bcfGewK+EEPU+J1P0MwXTLgpJ6r0VkzrKd/r0kVUrxXqhrMdSwtl6M3CgqDc3rFgiV5xs4nRjnwbhchud77ktZ3zV40uLYXHa5IlldN4O91MD1+LVffc5eceJmhn9ivuoEk+w/Wwtk8c/G2axakfmF9H4VFRgzyVnKrel2Gz4gZd1wihA2B8o4eh10pEmeS5O0BRDXpJGMC3FKCelX42mEYy4qr6bCF4Bqo0+bQOHgzZpdQQ+utmvrYlMVVcJMqh2xjSbaPdC+trOa0fvVBFTXIAF/Wn/1zFj6+G6mCYQ== talyz@zen-2018-11-16"
-      ];
+      openssh.authorizedKeys.keys = keys.talyz;
     };
 
     ozeloten = {
