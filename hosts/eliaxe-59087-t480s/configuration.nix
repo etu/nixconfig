bdc898a87d4a4332de5f5db9415d2e43f3a28d52
Elis Hirwing <elis@hirwing.se>
2019-09-20 10:14:21 +0200

    hosts/eliaxe-59087-t480s: Add new work laptop configuration

---
+++ hosts/eliaxe-59087-t480s/configuration.nix
@@ -0,0 +1,109 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  # Load secrets
+  secrets = import ../../data/load-secrets.nix;
+
+  # Declare download path for home-manager to avoid the need to have it as a channel
+  home-manager = builtins.fetchTarball {
+    url = "https://github.com/rycee/home-manager/archive/master.tar.gz";
+  };
+in {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./persistence.nix
+
+    # Import local modules
+    ../../modules
+
+    # Import the home-manager module
+    "${home-manager}/nixos"
+  ];
+
+  networking.hostName = "eliaxe-59087-t480s";
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable ZFS support
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "18582527";
+
+  # Install thinkpad modules for TLP
+  boot.extraModulePackages = [
+    pkgs.linuxPackages.acpi_call
+  ];
+
+  # Hardware settings
+  services.xserver.videoDrivers = ["modesetting"];
+  hardware.cpu.intel.updateMicrocode = true;
+
+  # Enable fwupd for firmware updates etc
+  services.fwupd.enable = true;
+
+  # Enable TLP
+  services.tlp.enable = true;
+
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable nfs server exports.
+  services.nfs.server.exports = ''
+    /home/etu/tvnu/projects 192.168.5.102(rw,no_subtree_check,all_squash,anonuid=1000,anongid=100)
+  '';
+
+  # Disable root login for ssh
+  services.openssh.permitRootLogin = "no";
+
+  # Enable aspell and hunspell with dictionaries.
+  my.spell.enable = true;
+
+  # Enable common cli settings for my systems
+  my.common-cli.enable = true;
+
+  # Enable gpg related stuff
+  my.gpg-utils.enable = true;
+
+  # Enable common graphical stuff
+  my.common-graphical.enable = true;
+
+  # Enable emacs deamon stuff
+  my.emacs.enable = true;
+
+  # Enable my exwm desktop settings
+  my.desktop-exwm.enable = true;
+
+  # Define a user account.
+  my.user.enable = true;
+
+  # Immutable users due to tmpfs
+  users.mutableUsers = false;
+
+  # Set passwords
+  users.users.root.initialHashedPassword = secrets.hashedEtuPassword;
+  users.users.etu.initialHashedPassword = secrets.hashedRootPassword;
+
+  # Enable nfsd with firewall rules.
+  my.nfsd.enable = true;
+
+  # Enable vbox and friends.
+  my.vbox.enable = true;
+
+  # Home-manager as nix module
+  home-manager.users.etu = import ../../home-etu-nixpkgs/home.nix;
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+}
---
+++ hosts/eliaxe-59087-t480s/persistence.nix
@@ -0,0 +1,28 @@
+{ ... }:
+
+{
+  environment.etc."machine-id".source = "/persistent/etc/machine-id";
+
+  environment.etc."ssh/ssh_host_rsa_key".source = "/persistent/etc/ssh/ssh_host_rsa_key";
+  environment.etc."ssh/ssh_host_rsa_key.pub".source = "/persistent/etc/ssh/ssh_host_rsa_key.pub";
+  environment.etc."ssh/ssh_host_ed25519_key".source = "/persistent/etc/ssh/ssh_host_ed25519_key";
+  environment.etc."ssh/ssh_host_ed25519_key.pub".source = "/persistent/etc/ssh/ssh_host_ed25519_key.pub";
+
+  # Avoid the need to have a moved config and help muscle memory of location
+  fileSystems."/etc/nixos" = {
+    device = "/persistent/etc/nixos";
+    options = [ "bind" "noauto" "x-systemd.automount" ];
+  };
+
+  # Persistence of NetworkManager network connections.
+  fileSystems."/etc/NetworkManager/system-connections/" = {
+    device = "/persistent/etc/NetworkManager/system-connections/";
+    options = [ "bind" "noauto" "x-systemd.automount" ];
+  };
+
+  # Persistence of roots dotfiles between boots
+  fileSystems."/root" = {
+    device = "/home/root";
+    options = [ "bind" "noauto" "x-systemd.automount" ];
+  };
+}
