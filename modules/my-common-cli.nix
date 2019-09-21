8c9a30e5c0827f0eba3f802d619299c1ed526bba
Elis Hirwing <elis@hirwing.se>
2019-09-21 15:51:59 +0200

    modules/common-cli: Install bat

--- modules/my-common-cli.nix
+++ modules/my-common-cli.nix
@@ -42,6 +42,7 @@ in {
     # List packages installed in system profile. To search by name, run:
     # $ nix-env -qaP | grep wget
     environment.systemPackages = with pkgs; [
+      bat
       bc                # Dependency for some fish functions
       curl
       file
