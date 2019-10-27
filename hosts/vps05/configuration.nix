5db70fda8b68255e2b43191b67247756d5dbd19d
Elis Hirwing <elis@hirwing.se>
2019-10-27 16:16:55 +0100

    hosts/vps05: Upgrade to 19.09

--- hosts/vps05/configuration.nix
+++ hosts/vps05/configuration.nix
@@ -39,7 +39,7 @@ in {
 
   # Auto upgrade system
   system.autoUpgrade.enable = true;
-  system.autoUpgrade.channel = "https://nixos.org/channels/nixos-19.03-small";
+  system.autoUpgrade.channel = "https://nixos.org/channels/nixos-19.09-small";
 
   # Auto garbage collect
   nix.gc.automatic = true;
