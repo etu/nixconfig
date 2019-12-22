012754c81865d40eb6d1923aeacd44faeeb9ea5a
Elis Hirwing <elis@hirwing.se>
2019-12-22 22:08:14 +0100

    hosts/fenchurch: Put NGiNX in front of home-assistant

--- hosts/fenchurch/services/hass.nix
+++ hosts/fenchurch/services/hass.nix
@@ -1,9 +1,25 @@
 { config, pkgs, ... }:
 
 {
+  # Make sure to have NGiNX enabled
+  services.nginx.enable = true;
+  services.nginx.virtualHosts."hass.lan" = {
+    listen = [ { addr = "0.0.0.0"; port = 81; } ];
+    locations."/".extraConfig = ''
+      proxy_pass http://127.0.0.1:8123;
+      proxy_http_version 1.1;
+      proxy_set_header Host $host;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection $connection_upgrade;
+    '';
+  };
+
+  # Open NGiNX port
+  networking.firewall.allowedTCPPorts = [ 81 ];
+
   # Enable Home Assistant, open port and add the hass user to the dialout group
   services.home-assistant.enable = true;
-  services.home-assistant.openFirewall = true;
   services.home-assistant.autoExtraComponents = false;
   services.home-assistant.package = pkgs.home-assistant.override {
     extraComponents = [
