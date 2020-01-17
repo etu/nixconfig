428ec6e2f656ff348672b3970af7d029982324b7
Elis Hirwing <elis@hirwing.se>
2020-01-17 20:55:54 +0100

    dotfiles: Reformat home.file attributes

--- home-etu-nixpkgs/home.nix
+++ home-etu-nixpkgs/home.nix
@@ -4,48 +4,34 @@
   programs.home-manager.enable = true;
   programs.home-manager.path = https://github.com/rycee/home-manager/archive/master.tar.gz;
 
-  home.file = [
+  home.file = {
     # Nano config
-    {
-      target = ".nanorc";
-      text = ''
-      set constantshow	    # Show linenumbers -c as default
-      '';
-    }
+    ".nanorc" = { text = "set constantshow # Show linenumbers -c as default"; };
 
-    # Mpv config file
-    {
-      target = ".config/mpv/mpv.conf";
-      text = ''
-      # Don't show images embedded in music files
-      no-audio-display
-      '';
-    }
+    # Mpv config file - Don't show images embedded in music files
+    ".config/mpv/mpv.conf" = { text = "no-audio-display"; };
 
     # Emacs inhibit startup screen
-    { target = ".emacs"; text = "(custom-set-variables '(inhibit-startup-screen t))"; }
+    ".emacs" = { text = "(custom-set-variables '(inhibit-startup-screen t))"; };
 
     # Tmux config
-    { target = ".tmux.conf"; source = ./dotfiles/tmux.conf; }
+    ".tmux.conf" = { source = ./dotfiles/tmux.conf; };
 
     # Fish config
-    { target = ".config/fish/config.fish"; source = ./dotfiles/fish/config.fish; }
+    ".config/fish/config.fish" = { source = ./dotfiles/fish/config.fish; };
 
     # Fish functions
-    { target = ".config/fish/functions"; source = ./dotfiles/fish/functions; }
+    ".config/fish/functions" = { source = ./dotfiles/fish/functions; };
 
     # Git config file for work
-    { target = ".config/git/work_config"; source = ./dotfiles/git/gitconfig_work; }
-
-    # Stupidterm
-    { target = ".config/stupidterm.ini"; source = ./dotfiles/stupidterm.ini; }
+    ".config/git/work_config" = { source = ./dotfiles/git/gitconfig_work; };
 
     # Kitty
-    { target = ".config/kitty/kitty.conf"; source = ./dotfiles/kitty.conf; }
+    ".config/kitty/kitty.conf" = { source = ./dotfiles/kitty.conf; };
 
     # GnuPG
-    { target = ".gnupg/dirmngr.conf"; text = "keyserver hkps://keys.openpgp.org"; }
-  ];
+    ".gnupg/dirmngr.conf" = { text = "keyserver hkps://keys.openpgp.org"; };
+  };
 
   programs.git = {
     enable = true;
