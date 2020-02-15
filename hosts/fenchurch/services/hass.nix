347487345b636f52cdc7f7ce3392fc3f46a76280
Elis Hirwing <elis@hirwing.se>
2020-02-15 22:20:20 +0100

    hosts/fenchurch/hass: Pin to specific commit of nixpkgs

--- hosts/fenchurch/services/hass.nix
+++ hosts/fenchurch/services/hass.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
-{
+let
+  hpkgs = import (builtins.fetchTarball {
+    url = https://github.com/NixOS/nixpkgs-channels/archive/b0bbacb52134a7e731e549f4c0a7a2a39ca6b481.tar.gz;
+  }) {};
+in {
   # Make sure to have NGiNX enabled
   services.nginx.enable = true;
   services.nginx.virtualHosts."hass.lan" = {
@@ -20,7 +24,7 @@
   # Enable Home Assistant, open port and add the hass user to the dialout group
   services.home-assistant.enable = true;
   services.home-assistant.autoExtraComponents = false;
-  services.home-assistant.package = pkgs.home-assistant.override {
+  services.home-assistant.package = hpkgs.home-assistant.override {
     extraComponents = [
       "cast"
       "discovery"
