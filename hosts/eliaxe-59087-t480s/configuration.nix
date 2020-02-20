f09b146ea0c779f1baf7e115517b4aa3154e32f5
Elis Hirwing <elis@hirwing.se>
2020-02-20 13:01:39 +0100

    hosts/work-laptop: Roll kernel back because of hardware issues

--- hosts/eliaxe-59087-t480s/configuration.nix
+++ hosts/eliaxe-59087-t480s/configuration.nix
@@ -30,7 +30,7 @@ in {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_5_4;
+  boot.kernelPackages = pkgs.linuxPackages_4_19;
 
   # Enable ZFS support
   boot.supportedFilesystems = [ "zfs" ];
