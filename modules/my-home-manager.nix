336237364fbec8c622ab6bfd25028c75f537b72c
Elis Hirwing <elis@hirwing.se>
2020-03-28 13:50:59 +0100

    modules/home: Rename compton to picom

--- modules/my-home-manager.nix
+++ modules/my-home-manager.nix
@@ -223,7 +223,7 @@ in {
       # Set up autorandr service to trigger on saved configurations
       programs.autorandr.enable = true;
 
-      services.compton.enable = true;
+      services.picom.enable = true;
       services.flameshot.enable = true;
       services.pasystray.enable = true;
       services.network-manager-applet.enable = true;
