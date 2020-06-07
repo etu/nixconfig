f835e7a5b1fbbcd7a3183b6c5e6c4d398cd65cbd
Elis Hirwing <elis@hirwing.se>
2020-06-07 19:39:30 +0200

    hosts/kodi: Migrate to the niv-imported persistence module from impermanence

--- hosts/kodi/persistence.nix
+++ hosts/kodi/persistence.nix
@@ -1,17 +1,17 @@
 { ... }:
 
 {
-  environment.etc."machine-id".source = "/persistent/etc/machine-id";
-
-  environment.etc."ssh/ssh_host_rsa_key".source = "/persistent/etc/ssh/ssh_host_rsa_key";
-  environment.etc."ssh/ssh_host_rsa_key.pub".source = "/persistent/etc/ssh/ssh_host_rsa_key.pub";
-  environment.etc."ssh/ssh_host_ed25519_key".source = "/persistent/etc/ssh/ssh_host_ed25519_key";
-  environment.etc."ssh/ssh_host_ed25519_key.pub".source = "/persistent/etc/ssh/ssh_host_ed25519_key.pub";
-
-  # Avoid the need to have a moved config and help muscle memory of location
-  fileSystems."/etc/nixos" = {
-    device = "/persistent/etc/nixos";
-    options = [ "bind" "noauto" "x-systemd.automount" ];
+  environment.persistence."/persistent" = {
+    directories = [
+      "/etc/nixos"
+    ];
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+    ];
   };
 
   # Persistence of roots dotfiles between boots
