d4a2b39203e4f2a9330d6f40cdd2e418357e60d5
Elis Hirwing <elis@hirwing.se>
2020-06-07 19:36:07 +0200

    hosts/fenchurch: Migrate to the niv-imported persistence module from impermanence

--- hosts/fenchurch/persistence.nix
+++ hosts/fenchurch/persistence.nix
@@ -1,15 +1,18 @@
 { ... }:
 
 {
-  environment.etc."machine-id".source = "/persistent/etc/machine-id";
-
-  environment.etc."ssh/ssh_host_rsa_key".source = "/persistent/etc/ssh/ssh_host_rsa_key";
-  environment.etc."ssh/ssh_host_rsa_key.pub".source = "/persistent/etc/ssh/ssh_host_rsa_key.pub";
-  environment.etc."ssh/ssh_host_ed25519_key".source = "/persistent/etc/ssh/ssh_host_ed25519_key";
-  environment.etc."ssh/ssh_host_ed25519_key.pub".source = "/persistent/etc/ssh/ssh_host_ed25519_key.pub";
-
-  # Avoid the need to have a moved config and help muscle memory of location
-  environment.etc."nixos".source = "/persistent/etc/nixos";
+  environment.persistence."/persistent" = {
+    directories = [
+      "/etc/nixos"
+    ];
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+    ];
+  };
 
   # Bind mount for persistent certificates for nginx
   fileSystems."/var/lib/acme" = {
