9a8e996c72be0feaed808315ff2d8f6c0325f040
Elis Hirwing <elis@hirwing.se>
2020-07-11 21:38:10 +0200

    hosts/fenchurch: Re-configure hass using nix

--- hosts/fenchurch/services/hass.nix
+++ hosts/fenchurch/services/hass.nix
@@ -2,7 +2,7 @@
 let
   hpkgs = import
     (builtins.fetchTarball {
-      url = https://github.com/NixOS/nixpkgs-channels/archive/b0bbacb52134a7e731e549f4c0a7a2a39ca6b481.tar.gz;
+      url = https://github.com/NixOS/nixpkgs/archive/f7334cad427d8eb80a51247a443cfc85514fdba0.tar.gz;
     }) { };
 in
 {
@@ -18,23 +18,101 @@ in
   };
 
   # Enable Home Assistant, open port and add the hass user to the dialout group
-  services.home-assistant.enable = true;
-  services.home-assistant.autoExtraComponents = false;
-  services.home-assistant.package = pkgs.home-assistant.override {
-    extraComponents = [
-      "cast"
-      "discovery"
-      "esphome"
-      "hue"
-      "kodi"
-      "media_player"
-      "mobile_app"
-      "notify"
-      "system_health"
-      "yr"
-      "zha"
-      "zwave"
-    ];
+  services.home-assistant = {
+    enable = true;
+    package = hpkgs.home-assistant;
+    config = {
+      # Basic settings
+      homeassistant = {
+        name = "Home";
+        latitude = "!secret lat_coord";
+        longitude = "!secret lon_coord";
+        elevation = 22;
+        unit_system = "metric";
+        time_zone = "Europe/Stockholm";
+      };
+
+      # Discover some devices automatically
+      discovery = { };
+
+      # Show some system health data
+      system_health = { };
+
+      # Http settings
+      http = {
+        server_host = "127.0.0.1";
+        base_url = "http://hass.lan";
+        use_x_forwarded_for = true;
+        trusted_proxies = "127.0.0.1";
+        server_port = 8123;
+      };
+
+      # Enables a map showing the location of tracked devies
+      map = { };
+
+      # Track the sun
+      sun = { };
+
+      # Include automations
+      automation = "!include automations.yaml";
+
+      # ZWave
+      zwave = {
+        usb_path = "/dev/serial/by-id/usb-0658_0200-if00";
+        network_key = "!secret zwave_network_key";
+      };
+
+      # ZHA
+      zha = {
+        usb_path = "/dev/serial/by-id/usb-dresden_elektronik_ingenieurtechnik_GmbH_ConBee_II_DE2124653-if00";
+        radio_type = "deconz";
+        database_path = "/var/lib/hass/zigbee.db";
+        enable_quirks = false;
+      };
+
+      # Enable mobile app
+      mobile_app = { };
+
+      # Enable configuration UI
+      config = { };
+
+      # Make the ui configurable through ui-lovelace.yaml
+      lovelace.mode = "yaml";
+
+      # Enable support for tracking state changes over time
+      history = { };
+
+      # Purge tracked history after 10 days
+      recorder.purge_keep_days = 10;
+
+      # View all events in o logbook
+      logbook = { };
+
+      # Enable logging
+      logger = {
+        default = "info";
+        logs = {
+          "bellows.ezsp" = "debug";
+          "bellows.zigbee.application" = "debug";
+          "homeassistant.components.zha" = "debug";
+          "homeassistant.core" = "debug";
+          "mobile_app" = "debug";
+          "zhaquirks" = "debug";
+          "zigpy" = "debug";
+          "zigpy_cc" = "debug";
+          "zigpy_deconz.api" = "debug";
+          "zigpy_deconz.zigbee.application" = "debug";
+          "zigpy_xbee.api" = "debug";
+          "zigpy_xbee.zigbee.application" = "debug";
+          "zigpy_zigate" = "debug";
+        };
+      };
+
+      # Sensors
+      sensor = [
+        { platform = "yr"; name = "Weather"; }
+      ];
+    };
   };
 
   users.users.hass.extraGroups = [ "dialout" ];
