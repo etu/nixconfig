63dc5d218b08e2cae9a75840b5da4257f8ac2da4
Elis Hirwing <elis@hirwing.se>
2020-08-03 10:16:34 +0200

    modules/emacs: Restructure exwm config again

--- modules/emacs-files/exwm.el
+++ modules/emacs-files/exwm.el
@@ -4,6 +4,75 @@
 
 (require 'use-package)
 
+
+;; Configure EXWM
+(use-package exwm
+  :ensure t
+  :config
+  (progn
+    ;; Define functions to easily run commands
+    (defun exwm-run (command)
+      "Run COMMAND."
+      (interactive (list (read-shell-command "> ")))
+      (start-process-shell-command command nil command))
+
+    (defun exwm-run-systemd (command)
+      (interactive (list (read-shell-command "$ ")))
+      (let ((cmd (concat "@systemd@/bin/systemd-run --user " command)))
+        (start-process-shell-command cmd nil cmd)))
+
+    (defun exwm-run-terminal ()
+      (interactive)
+      (exwm-run-systemd "@kitty@/bin/kitty"))
+
+    (defun exwm-run-rofi-emoji ()
+      (interactive)
+      (exwm-run "@rofi@/bin/rofi -show emoji -theme glue_pro_blue"))
+
+    ;;
+    ;; Bind keys
+    ;;
+    (exwm-input-set-key (kbd "s-SPC") 'exwm-run-systemd)
+    (exwm-input-set-key (kbd "s-e") 'exwm-run-systemd)
+    (exwm-input-set-key (kbd "C-s-SPC") 'exwm-run)
+    (exwm-input-set-key (kbd "s-t") 'exwm-run-terminal)
+    (exwm-input-set-key (kbd "s-u") 'exwm-run-rofi-emoji)
+
+    ;; Keybinds to switch workspaces
+    (dotimes (i 10)
+      (exwm-input-set-key (kbd (format "s-%d" i))
+                          `(lambda ()
+                             (interactive)
+                             (exwm-workspace-switch-create ,i))))
+
+    ;; Make it possible to do exwm-reset to get out of char-mode
+    (exwm-input-set-key (kbd "s-r") 'exwm-input-grab-keyboard)
+
+    ;;
+    ;; Some EXWM hooks to make things "nicer" overall
+    ;;
+    (add-hook 'exwm-update-title-hook
+              (lambda ()
+                (let ((tilde-exwm-title
+                       (replace-regexp-in-string (getenv "HOME") "~" exwm-title)))
+                  (exwm-workspace-rename-buffer (format "%s: %s" exwm-class-name tilde-exwm-title)))))
+
+    (add-hook 'exwm-update-class-hook
+              (lambda ()
+                (exwm-workspace-rename-buffer exwm-class-name)))))
+
+
+ ;; Display battery mode
+(use-package battery
+  :config (add-hook 'exwm-init-hook 'display-battery-mode))
+
+
+;; Display time in modeline
+(use-package time
+  :init (setq display-time-24hr-format t)
+  :config (add-hook 'exwm-init-hook 'display-time-mode))
+
+
 ;; Define desktop environment commands
 (use-package desktop-environment
   :ensure t
@@ -26,72 +95,21 @@
          desktop-environment-brightness-small-decrement "-dec 5")
   :config (desktop-environment-mode 1))
 
-;; Display time in modeline
-(use-package time
-  :init (setq display-time-24hr-format t)
-  :config (add-hook 'exwm-init-hook 'display-time-mode))
 
-;; Display battery mode
-(use-package battery
-  :config (add-hook 'exwm-init-hook 'display-battery-mode))
+;; Enable randr support
+(use-package exwm-randr
+  :config (exwm-randr-enable))
 
-;; Define a function to easily run commands
+
+;; Enable systemtray
+(use-package exwm-systemtray
+  :config (exwm-systemtray-enable))
+
+
+;; Load EXWM
 (use-package exwm
-  :ensure t
   :config
   (progn
-    (defun exwm-run-systemd (command)
-      (interactive (list (read-shell-command "$ ")))
-      (let ((cmd (concat "@systemd@/bin/systemd-run --user " command)))
-        (start-process-shell-command cmd nil cmd)))
-    (exwm-input-set-key (kbd "s-SPC") 'exwm-run-systemd)
-    (exwm-input-set-key (kbd "s-e") 'exwm-run-systemd)
-
-    (defun exwm-run (command)
-      "Run COMMAND."
-      (interactive (list (read-shell-command "> ")))
-      (start-process-shell-command command nil command))
-    (exwm-input-set-key (kbd "C-s-SPC") 'exwm-run)
-
-    ;; Special function to run the terminal
-    (defun exwm-run-terminal ()
-      (interactive)
-      (exwm-run-systemd "@kitty@/bin/kitty"))
-    (exwm-input-set-key (kbd "s-t") 'exwm-run-terminal)
-
-    (defun exwm-run-rofi-emoji ()
-      (interactive)
-      (exwm-run "@rofi@/bin/rofi -show emoji -theme glue_pro_blue"))
-    (exwm-input-set-key (kbd "s-u") 'exwm-run-rofi-emoji)
-
-    ;; Bind switch to workspace commands
-    (dotimes (i 10)
-      (exwm-input-set-key (kbd (format "s-%d" i))
-                          `(lambda ()
-                             (interactive)
-                             (exwm-workspace-switch-create ,i))))
-
-    ;; Make it possible to do exwm-reset
-    (exwm-input-set-key (kbd "s-r") 'exwm-input-grab-keyboard)
-
-    ;; Some hooks to tweak exwm
-    (add-hook 'exwm-update-class-hook
-              (lambda ()
-                (exwm-workspace-rename-buffer exwm-class-name)))
-
-    (add-hook 'exwm-update-title-hook
-              (lambda ()
-                (let ((tilde-exwm-title
-                       (replace-regexp-in-string (getenv "HOME") "~" exwm-title)))
-                  (exwm-workspace-rename-buffer (format "%s: %s" exwm-class-name tilde-exwm-title)))))
-
-    ;; Enable randr
-    (use-package exwm-randr :config (exwm-randr-enable))
-
-    ;; Enable systray
-    (use-package exwm-systemtray :config (exwm-systemtray-enable))
-
-    ;; Start exwm
     (exwm-enable)
     (exwm-init)
 
