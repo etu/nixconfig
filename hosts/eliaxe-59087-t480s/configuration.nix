e5b73a2ed66cc85092129f0cff3cd4798c501233
Elis Hirwing <elis@hirwing.se>
2020-08-10 08:02:31 +0200

    hosts/eliaxe-59087-t480s: Upgrade kernel

--- hosts/eliaxe-59087-t480s/configuration.nix
+++ hosts/eliaxe-59087-t480s/configuration.nix
@@ -29,7 +29,7 @@ in
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_5_6;
+  boot.kernelPackages = pkgs.linuxPackages_5_7;
 
   # Enable ZFS support
   boot.supportedFilesystems = [ "zfs" ];
