1b4a2f8fb72d25916d56088280c8a2d6717e3491
Elis Hirwing <elis@hirwing.se>
2020-08-19 17:13:54 +0200

    modules/i3: Organize config a bit

--- modules/my-i3.nix
+++ modules/my-i3.nix
@@ -11,175 +11,212 @@ let
     #
     # Please see https://i3wm.org/docs/userguide.html for a complete reference!
 
-    set $mod Mod4
+    ##
+    ## Variables
+    ##
+      set $mod Mod4
 
-    # Font for window titles. Will also be used by the bar unless a different font
-    # is used in the bar {} block below.
-    font pango:monospace 8
+      # Direction keys (Emacs logic)
+      set $left b
+      set $right f
+      set $up p
+      set $down n
 
-    # Use Mouse+$mod to drag floating windows to their wanted position
-    floating_modifier $mod
+      # Terminal emulator
+      set $term ${pkgs.kitty}/bin/kitty
 
-    # Start a terminal
-    bindsym $mod+Return exec ${cfg.package}/bin/i3-sensible-terminal
+    ##
+    ## Key bindings
+    ##
 
-    # Kill focused window
-    bindsym $mod+Shift+quotedbl kill
+    #
+    # Basics:
+    #
+      # Start a terminal:
+      bindsym $mod+Return exec $term
 
-    # Start rofi (to launch and select programs)
-    bindsym $mod+e exec ${rofi}/bin/rofi -show combi -theme glue_pro_blue
+      # Kill focused window:
+      bindsym $mod+Shift+quotedbl kill
 
-    # Change focus (Emacs style)
-    bindsym $mod+b focus left
-    bindsym $mod+f focus right
-    bindsym $mod+p focus up
-    bindsym $mod+n focus down
+      # Start your launcher:
+      bindsym $mod+e exec ${rofi}/bin/rofi -show combi -theme glue_pro_blue
 
-    # Alternatively, you can use the cursor keys:
-    bindsym $mod+Left focus left
-    bindsym $mod+Down focus down
-    bindsym $mod+Up focus up
-    bindsym $mod+Right focus right
+      # Use Mouse+$mod to drag floating windows to their wanted position:
+      floating_modifier $mod
 
-    # Move focused window (Emacs style)
-    bindsym $mod+Shift+b move left
-    bindsym $mod+Shift+f move right
-    bindsym $mod+Shift+p move up
-    bindsym $mod+Shift+n move down
+      # Reload the configuration file:
+      bindsym $mod+Shift+c reload
 
-    # Alternatively, you can use the cursor keys:
-    bindsym $mod+Shift+Left move left
-    bindsym $mod+Shift+Down move down
-    bindsym $mod+Shift+Up move up
-    bindsym $mod+Shift+Right move right
+      # Restart i3 inplace:
+      bindsym $mod+Shift+l restart
 
-    # Split in horizontal orientation
-    bindsym $mod+h split h
+      # Exit i3 (logs you out of your X session):
+      bindsym $mod+Shift+greater exec "${cfg.package}/bin/i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' '${cfg.package}/bin/i3-msg exit'"
 
-    # Split in vertical orientation
-    bindsym $mod+v split v
+    #
+    # Moving around:
+    #
+      # Move focus around:
+      bindsym $mod+$left focus left
+      bindsym $mod+$right focus right
+      bindsym $mod+$up focus up
+      bindsym $mod+$down focus down
 
-    # Fullscreen for the focused container
-    bindsym $mod+u fullscreen toggle
+      # Move focus around with cursor keys:
+      bindsym $mod+Left focus left
+      bindsym $mod+Down focus down
+      bindsym $mod+Up focus up
+      bindsym $mod+Right focus right
 
-    # Change container layout (stacked, tabbed, toggle split)
-    bindsym $mod+o layout stacking
-    bindsym $mod+comma layout tabbed
-    bindsym $mod+period layout toggle split
+      # Move focused window:
+      bindsym $mod+Shift+$left move left
+      bindsym $mod+Shift+$right move right
+      bindsym $mod+Shift+$up move up
+      bindsym $mod+Shift+$down move down
 
-    # Toggle tiling / floating
-    bindsym $mod+Shift+space floating toggle
+      # Move focused window with cursor keys:
+      bindsym $mod+Shift+Left move left
+      bindsym $mod+Shift+Down move down
+      bindsym $mod+Shift+Up move up
+      bindsym $mod+Shift+Right move right
 
-    # Change focus between tiling / floating windows
-    bindsym $mod+space focus mode_toggle
+    #
+    # Workspaces:
+    #
+      # Define the names for the workspaces we want to create:
+      set $ws1 "1 - Misc"
+      set $ws2 "2 - Web"
+      set $ws3 "3 - Emacs"
+      set $ws4 "4"
+      set $ws5 "5"
+      set $ws6 "6"
+      set $ws7 "7"
+      set $ws8 "8"
+      set $ws9 "9"
+      set $ws10 "10"
 
-    # Focus the parent container
-    bindsym $mod+a focus parent
+      # Switch to workspace:
+      bindsym $mod+1 workspace number $ws1
+      bindsym $mod+2 workspace number $ws2
+      bindsym $mod+3 workspace number $ws3
+      bindsym $mod+4 workspace number $ws4
+      bindsym $mod+5 workspace number $ws5
+      bindsym $mod+6 workspace number $ws6
+      bindsym $mod+7 workspace number $ws7
+      bindsym $mod+8 workspace number $ws8
+      bindsym $mod+9 workspace number $ws9
+      bindsym $mod+0 workspace number $ws10
 
-    # Focus the child container
-    bindsym $mod+d focus child
+      # Move focused container to workspace:
+      bindsym $mod+Shift+1 move container to workspace number $ws1
+      bindsym $mod+Shift+2 move container to workspace number $ws2
+      bindsym $mod+Shift+3 move container to workspace number $ws3
+      bindsym $mod+Shift+4 move container to workspace number $ws4
+      bindsym $mod+Shift+5 move container to workspace number $ws5
+      bindsym $mod+Shift+6 move container to workspace number $ws6
+      bindsym $mod+Shift+7 move container to workspace number $ws7
+      bindsym $mod+Shift+8 move container to workspace number $ws8
+      bindsym $mod+Shift+9 move container to workspace number $ws9
+      bindsym $mod+Shift+0 move container to workspace number $ws10
 
-    # Define names for default workspaces for which we configure key bindings later on.
-    # We use variables to avoid repeating the names in multiple places.
-    set $ws1 "1 - Misc"
-    set $ws2 "2 - Web"
-    set $ws3 "3 - Emacs"
-    set $ws4 "4"
-    set $ws5 "5"
-    set $ws6 "6"
-    set $ws7 "7"
-    set $ws8 "8"
-    set $ws9 "9"
-    set $ws10 "10"
+    #
+    # Layouting:
+    #
+      # Split in horizontal orientation:
+      bindsym $mod+h split h
 
-    # Switch to workspace
-    bindsym $mod+1 workspace number $ws1
-    bindsym $mod+2 workspace number $ws2
-    bindsym $mod+3 workspace number $ws3
-    bindsym $mod+4 workspace number $ws4
-    bindsym $mod+5 workspace number $ws5
-    bindsym $mod+6 workspace number $ws6
-    bindsym $mod+7 workspace number $ws7
-    bindsym $mod+8 workspace number $ws8
-    bindsym $mod+9 workspace number $ws9
-    bindsym $mod+0 workspace number $ws10
+      # Split in vertical orientation:
+      bindsym $mod+v split v
 
-    # Move focused container to workspace
-    bindsym $mod+Shift+1 move container to workspace number $ws1
-    bindsym $mod+Shift+2 move container to workspace number $ws2
-    bindsym $mod+Shift+3 move container to workspace number $ws3
-    bindsym $mod+Shift+4 move container to workspace number $ws4
-    bindsym $mod+Shift+5 move container to workspace number $ws5
-    bindsym $mod+Shift+6 move container to workspace number $ws6
-    bindsym $mod+Shift+7 move container to workspace number $ws7
-    bindsym $mod+Shift+8 move container to workspace number $ws8
-    bindsym $mod+Shift+9 move container to workspace number $ws9
-    bindsym $mod+Shift+0 move container to workspace number $ws10
+      # Change layout of focused container:
+      bindsym $mod+o layout stacking
+      bindsym $mod+comma layout tabbed
+      bindsym $mod+period layout toggle split
 
-    # Printscreen
-    bindsym Print exec ${pkgs.flameshot}/bin/flameshot gui
+      # Fullscreen for the focused container:
+      bindsym $mod+u fullscreen toggle
 
-    # Backlight
-    bindsym XF86MonBrightnessUp exec ${pkgs.acpilight}/bin/xbacklight -inc 10
-    bindsym XF86MonBrightnessDown exec ${pkgs.acpilight}/bin/xbacklight -dec 10
+      # Toggle the current focus between tiling and floating mode:
+      bindsym $mod+Shift+space floating toggle
 
-    # Audio
-    bindsym XF86AudioMute exec pactl set-sink-mute @DEFAULT_SINK@ toggle
-    bindsym XF86AudioLowerVolume exec pactl set-sink-volume @DEFAULT_SINK@ -10%
-    bindsym XF86AudioRaiseVolume exec pactl set-sink-volume @DEFAULT_SINK@ +10%
-    bindsym XF86AudioMicMute exec pactl set-source-mute @DEFAULT_SOURCE@ toggle
+      # Swap focus between the tiling area and the floating area:
+      bindsym $mod+space focus mode_toggle
 
-    # Misc buttons
-    bindsym XF86Tools exec emacs
-    bindsym XF86Display exec ${pkgs.autorandr}/bin/autorandr -cf
-    bindsym XF86Favorites exec emacs
+      # Focus the parent container
+      bindsym $mod+a focus parent
 
-    # Rofi emoji picker
-    bindsym $mod+i exec ${rofi}/bin/rofi -show emoji -theme glue_pro_blue
+      # Focus the child container
+      bindsym $mod+d focus child
 
-    # Restart i3 inplace (preserves your layout/session, can be used to upgrade i3)
-    bindsym $mod+Shift+l restart
-
-    # Exit i3 (logs you out of your X session)
-    bindsym $mod+Shift+greater exec "${cfg.package}/bin/i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' '${cfg.package}/bin/i3-msg exit'"
-
-    # Resize window (you can also use the mouse for that)
-    mode "resize" {
+    #
+    # Resizing:
+    #
       # These bindings trigger as soon as you enter the resize mode
+      mode "resize" {
+        # Pressing left will shrink the window’s width.
+        # Pressing right will grow the window’s width.
+        # Pressing up will shrink the window’s height.
+        # Pressing down will grow the window’s height.
+        bindsym $left resize shrink width 10px
+        bindsym $right resize grow width 10px
+        bindsym $up resize shrink height 10px
+        bindsym $down resize grow height 10px
 
-      # Pressing left will shrink the window’s width.
-      # Pressing right will grow the window’s width.
-      # Pressing up will shrink the window’s height.
-      # Pressing down will grow the window’s height.
-      bindsym b resize shrink width 10 px or 10 ppt
-      bindsym f resize grow width 10 px or 10 ppt
-      bindsym n resize grow height 10 px or 10 ppt
-      bindsym p resize shrink height 10 px or 10 ppt
+        # You can also use the arrow keys:
+        bindsym Left resize shrink width 10 px or 10 ppt
+        bindsym Down resize grow height 10 px or 10 ppt
+        bindsym Up resize shrink height 10 px or 10 ppt
+        bindsym Right resize grow width 10 px or 10 ppt
 
-      # same bindings, but for the arrow keys
-      bindsym Left resize shrink width 10 px or 10 ppt
-      bindsym Down resize grow height 10 px or 10 ppt
-      bindsym Up resize shrink height 10 px or 10 ppt
-      bindsym Right resize grow width 10 px or 10 ppt
+        # Exit resize mode:
+        bindsym Return mode "default"
+        bindsym Escape mode "default"
+        bindsym $mod+r mode "default"
+      }
 
-      # back to normal: Enter or Escape or $mod+r
-      bindsym Return mode "default"
-      bindsym Escape mode "default"
-      bindsym $mod+r mode "default"
-    }
+      # Enter resize mode:
+      bindsym $mod+r mode "resize"
 
-    bindsym $mod+r mode "resize"
+    #
+    # Other keybindings:
+    #
+      # Printscreen:
+      bindsym Print exec ${pkgs.flameshot}/bin/flameshot gui
 
-    bindsym $mod+l exec "${physlockCommand}"
+      # Backlight:
+      bindsym XF86MonBrightnessUp exec ${pkgs.acpilight}/bin/xbacklight -inc 10
+      bindsym XF86MonBrightnessDown exec ${pkgs.acpilight}/bin/xbacklight -dec 10
 
-    # Start i3bar to display a workspace bar (plus the system information i3status
-    # finds out, if available)
-    bar {
-      status_command i3status
-      tray_output primary
-      position top
-      font pango:monospace 10
-    }
+      # Audio:
+      bindsym XF86AudioMute exec pactl set-sink-mute @DEFAULT_SINK@ toggle
+      bindsym XF86AudioLowerVolume exec pactl set-sink-volume @DEFAULT_SINK@ -10%
+      bindsym XF86AudioRaiseVolume exec pactl set-sink-volume @DEFAULT_SINK@ +10%
+      bindsym XF86AudioMicMute exec pactl set-source-mute @DEFAULT_SOURCE@ toggle
+
+      # Misc buttons:
+      bindsym XF86Tools exec emacs
+      bindsym XF86Display exec ${pkgs.autorandr}/bin/autorandr -cf
+      bindsym XF86Favorites exec emacs
+
+      # Rofi emoji picker:
+      bindsym $mod+i exec ${rofi}/bin/rofi -show emoji -theme glue_pro_blue
+
+      # Lock the screen:
+      bindsym $mod+l exec "${physlockCommand}"
+
+    ##
+    ## Visuals
+    ##
+      # Font for window titles:
+      font pango:monospace 8
+
+      # i3bar settings:
+      bar {
+        status_command i3status
+        tray_output primary
+        position top
+        font pango:monospace 10
+      }
   '';
 
 in {
