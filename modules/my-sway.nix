9bb2ec744b04b6ba81a31b559fa75ed5bbfae5dd
Elis Hirwing <elis@hirwing.se>
2020-09-04 18:53:45 +0200

    modules/sway: Reformat xkb_keymap

--- modules/my-sway.nix
+++ modules/my-sway.nix
@@ -19,19 +19,19 @@ let
     // parts that gets included are available in the different subfolders in:
     // ${pkgs.xorg.xkeyboardconfig}/share/X11/xkb/
     xkb_keymap {
-    	xkb_keycodes  { include "evdev+aliases(qwerty)"	};
-    	xkb_types     { include "default"	};
-    	xkb_compat    { include "complete"	};
-    	xkb_symbols   {
-    		include "pc+us(dvorak)+inet(evdev)+ctrl(nocaps)+eurosign(e)+kpdl(dot)"
+      xkb_keycodes { include "evdev+aliases(qwerty)" };
+      xkb_types    { include "default" };
+      xkb_compat   { include "complete" };
+      xkb_symbols  {
+        include "pc+us(dvorak)+inet(evdev)+ctrl(nocaps)+eurosign(e)+kpdl(dot)"
 
-    		// Less than/Greater than/Pipe key on Swedish keyboards becomes Compose
-    		replace key <LSGT> { [ Multi_key ] };
+        // Less than/Greater than/Pipe key on Swedish keyboards becomes Compose
+        replace key <LSGT> { [ Multi_key ] };
 
-    		// Scroll Lock becomes Compose
-    		replace key <SCLK> { [ Multi_key ] };
-    	};
-    	xkb_geometry  { include "pc(pc105)"	};
+        // Scroll Lock becomes Compose
+        replace key <SCLK> { [ Multi_key ] };
+      };
+      xkb_geometry { include "pc(pc105)" };
     };
   '';
 
