301524b0694a59b69b6859334f93ab1f3669f202
Elis Hirwing <elis@hirwing.se>
2020-09-07 15:11:55 +0200

    modules/sway: Make Firefox Picture-in-Picture window float

--- modules/my-sway.nix
+++ modules/my-sway.nix
@@ -296,6 +296,7 @@ let
       for_window [app_id="kitty"] border pixel 3
       for_window [app_id="firefox"] border pixel 3
       for_window [app_id="firefox" title="Firefox - Sharing Indicator"] floating enable
+      for_window [app_id="firefox" title="Picture-in-Picture"] floating enable
       for_window [class="Brave-browser"] border pixel 3
       for_window [class="Chromium-browser"] border pixel 3
       for_window [class="Emacs"] border pixel 3
