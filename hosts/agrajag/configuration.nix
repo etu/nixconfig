d810a77b4b5d9b7189386cb069bfb17a35698cb8
Elis Hirwing <elis@hirwing.se>
2020-10-23 23:39:47 +0200

    hosts/agrajag: Upgrade kernel to 5.9

--- hosts/agrajag/configuration.nix
+++ hosts/agrajag/configuration.nix
@@ -38,7 +38,7 @@ in
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_5_8;
+  boot.kernelPackages = pkgs.linuxPackages_5_9;
 
   # Settings needed for ZFS
   boot.supportedFilesystems = [ "zfs" ];
