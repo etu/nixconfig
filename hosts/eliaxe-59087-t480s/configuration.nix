d53cc6e678bc6262e9ebaea38f9ca88d30c73d4f
Elis Hirwing <elis@hirwing.se>
2020-11-13 14:00:11 +0100

    hosts/eliaxe-59087-t480s: Upgrade from Linux 4.8 to 4.9

--- hosts/eliaxe-59087-t480s/configuration.nix
+++ hosts/eliaxe-59087-t480s/configuration.nix
@@ -30,7 +30,7 @@ in
     # Use the systemd-boot EFI boot loader.
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
-    boot.kernelPackages = pkgs.linuxPackages_5_8;
+    boot.kernelPackages = pkgs.linuxPackages_5_9;
 
     # Enable ZFS support
     boot.supportedFilesystems = [ "zfs" ];
