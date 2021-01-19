3eb8fce3a6c75a96c8c318efe65299fa7ab6531d
Elis Hirwing <elis@hirwing.se>
2021-01-19 17:09:57 +0100

    hosts/fenchurch/hass: Migrate all automations to be declared by nix

--- hosts/fenchurch/services/hass.nix
+++ hosts/fenchurch/services/hass.nix
@@ -55,7 +55,156 @@ in
       sun = { };
 
       # Include automations
-      automation = "!include automations.yaml";
+      automation = [
+        # Turn on the LED strip in the evening
+        {
+          id = "turn-on-evening-lights";
+          alias = "Turn on evening lights";
+          trigger = [
+            {
+              platform = "sun";
+              event = "sunset";
+              offset = "-00:45:00";
+            }
+          ];
+          action.data.entity_id = [ "light.tv_wall_strip" ];
+          action.service = "light.turn_on";
+        }
+
+        # Turn on the floor lamps in the evening
+        {
+          id = "turn-on-evening-switches";
+          alias = "Turn on evening switches";
+          trigger = [
+            {
+              platform = "sun";
+              event = "sunset";
+              offset = "-00:45:00";
+            }
+          ];
+          action.data.entity_id = [ "switch.floorlamp_office" "switch.floorlamp_livingroom" ];
+          action.service = "switch.turn_on";
+        }
+
+        # Turn off the floor lamps in the evening
+        {
+          id = "turn-off-evening-switches";
+          alias = "Turn off evening switches";
+          trigger = [
+            { platform = "time"; at = "00:00:00"; }
+            {
+              platform = "state";
+              entity_id = "switch.floorlamp_livingroom";
+              to = "on";
+              for.minutes = 30;
+            }
+            {
+              platform = "state";
+              entity_id = "switch.floorlamp_office";
+              to = "on";
+              for.minutes = 30;
+            }
+          ];
+          condition = [
+            {
+              condition = "time";
+              after = "00:00:00";
+              before = "10:00:00";
+            }
+          ];
+          action.data.entity_id = [ "switch.floorlamp_office" "switch.floorlamp_livingroom" ];
+          action.service = "switch.turn_off";
+        }
+
+        # Turn off the LED strip in the evening
+        {
+          id = "turn-off-tv-wall-strip";
+          alias = "Turn off TV Wall Strip";
+          trigger = [
+            { platform = "time"; at = "01:30:00"; }
+          ];
+          action.data.entity_id = [ "light.tv_wall_strip" ];
+          action.service = "light.turn_off";
+        }
+
+        # Turn off hallway ceiling lamps timers
+        {
+          id = "turn-off-hallway-ceilinglamp-1-timer";
+          alias = "Turn off hallway ceilinglamp 1 timer";
+          trigger = [
+            {
+              platform = "state";
+              entity_id = "light.ceilinglamp_hallway_1";
+              for.minutes = 20;
+              to = "on";
+            }
+          ];
+          action.data.entity_id = [ "light.ceilinglamp_hallway_1" ];
+          action.service = "light.turn_off";
+        }
+        {
+          id = "turn-off-hallway-ceilinglamp-2-timer";
+          alias = "Turn off hallway ceilinglamp 2 timer";
+          trigger = [
+            {
+              platform = "state";
+              entity_id = "light.ceilinglamp_hallway_2";
+              for.minutes = 15;
+              to = "on";
+            }
+          ];
+          action.data.entity_id = [ "light.ceilinglamp_hallway_2" ];
+          action.service = "light.turn_off";
+        }
+
+        # Turn on the other hallway lamp
+        {
+          id = "turn-on-other-hallway-ceilinglamp-1";
+          alias = "Turn on other hallway ceilinglamp 1";
+          trigger = [
+            {
+              platform = "state";
+              entity_id = "light.ceilinglamp_hallway_1";
+              to = "on";
+            }
+          ];
+          action.data.entity_id = [ "light.ceilinglamp_hallway_2" ];
+          action.service = "light.turn_on";
+        }
+        {
+          id = "turn-on-other-hallway-ceilinglamp-2";
+          alias = "Turn on other hallway ceilinglamp 2";
+          trigger = [
+            {
+              platform = "state";
+              entity_id = "light.ceilinglamp_hallway_2";
+              to = "on";
+            }
+          ];
+          action.data.entity_id = [ "light.ceilinglamp_hallway_1" ];
+          action.service = "light.turn_on";
+        }
+
+        # Turn on media center power for updates in the evening
+        {
+          id = "turn-on-media-center-power-for-updates";
+          alias = "Turn on media center power for updates";
+          trigger = [
+            { platform = "time"; at = "00:55:00"; }
+          ];
+          action.data.entity_id = [ "switch.media_center_power" ];
+          action.service = "switch.turn_on";
+        }
+        {
+          id = "turn-off-media-center-power";
+          alias = "Turn off media center power";
+          trigger = [
+            { platform = "time"; at = "02:30:00"; }
+          ];
+          action.data.entity_id = [ "switch.media_center_power" ];
+          action.service = "switch.turn_off";
+        }
+      ];
 
       # Include scripts
       script = "!include scripts.yaml";
