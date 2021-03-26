cbccfb8a11f276ce862411d4842695cb8a134780
Elis Hirwing <elis@hirwing.se>
2021-03-26 15:27:27 +0100

    hosts/fenchurch: Copy the Grub bootloader to Microsoft path after install
    My EFI is incredibly buggy so it magically "forgets" of all other
    bootloaders than Microsofts bootloader each reboot. Even if you remove
    Microsofts bootloader it will re-appear to be the default bootloader.
    
    With a recent NixOS upgrade I got a new Grub and new Grub modules, but
    the module needed for the one I had manually copied to the Microsoft
    path years ago didn't get updated and therefore didn't work with these
    new modules.
    
    So this will fix this for the future, make sure to always update the
    Microsoft bootloader path so it's never out of date again.

--- hosts/fenchurch/configuration.nix
+++ hosts/fenchurch/configuration.nix
@@ -42,6 +42,11 @@ in
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.device = "nodev";
+  boot.loader.grub.extraInstallCommands = ''
+    mkdir -p /boot/EFI/Microsoft/Boot/ /boot-fallback/EFI/Microsoft/Boot/
+    cp /boot/EFI/grub/grubx64.efi /boot/EFI/Microsoft/Boot/bootmgfw.efi
+    cp /boot-fallback/EFI/grub/grubx64.efi /boot-fallback/EFI/Microsoft/Boot/bootmgfw.efi
+  '';
 
   boot.loader.grub.mirroredBoots = [
     { devices = [ "/dev/disk/by-uuid/6258-01A0" ]; path = "/boot-fallback"; }
