c00136285b98caabef13e7e04fa95012f5a55f84
Elis Hirwing <elis@hirwing.se>
2021-07-11 20:28:52 +0200

    hosts/fenchurch: Stop mounting /etc/nixos

--- hosts/fenchurch/persistence.nix
+++ hosts/fenchurch/persistence.nix
@@ -1,18 +1,13 @@
 { ... }:
 
 {
-  environment.persistence."/persistent" = {
-    directories = [
-      "/etc/nixos"
-    ];
-    files = [
-      "/etc/machine-id"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-    ];
-  };
+  environment.persistence."/persistent".files = [
+    "/etc/machine-id"
+    "/etc/ssh/ssh_host_rsa_key"
+    "/etc/ssh/ssh_host_rsa_key.pub"
+    "/etc/ssh/ssh_host_ed25519_key"
+    "/etc/ssh/ssh_host_ed25519_key.pub"
+  ];
 
   # Bind mount for persistent certificates for nginx
   fileSystems."/var/lib/acme" = {
