08c820eb364abacc1334560104543a1582bb8e50
Elis Hirwing <elis@hirwing.se>
2021-07-11 20:49:16 +0200

    deploy: merge some files for easier use

--- deploy/public.nix
+++ deploy/public.nix
@@ -9,6 +9,22 @@ import nixusPath { } ({ config, ... }: {
     nixpkgs = ../nix/nixos-unstable;
   };
 
+  nodes.fenchurch = { lib, config, ... }: {
+    # How to reach this node
+    host = "deploy@home.elis.nu";
+
+    # What configuration it should have
+    configuration = ../hosts/fenchurch/configuration.nix;
+  };
+
+  nodes.vps04 = { lib, config, ... }: {
+    # How to reach this node
+    host = "deploy@vps04.elis.nu";
+
+    # What configuration it should have
+    configuration = ../hosts/vps04/configuration.nix;
+  };
+
   nodes.vps05 = { lib, config, ... }: {
     # How to reach this node
     host = "deploy@vps05.elis.nu";
