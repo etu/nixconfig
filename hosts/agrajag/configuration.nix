e554f314bec3e2d0cd5cb2a5f477e559550b60e1
Elis Hirwing <elis@hirwing.se>
2021-07-11 22:00:09 +0200

    hosts/agrajag: Add cachix substituters for fossar/nix-phps and nix-community

--- hosts/agrajag/configuration.nix
+++ hosts/agrajag/configuration.nix
@@ -140,6 +140,9 @@ in {
   }];
   nix.distributedBuilds = true;
   nix.extraOptions = ''
-		builders-use-substitutes = true
+    builders-use-substitutes = true
+
+    substituters = https://cache.nixos.org https://cache.nixos.org/ https://fossar.cachix.org https://nix-community.cachix.org
+    trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= fossar.cachix.org-1:Zv6FuqIboeHPWQS7ysLCJ7UT7xExb4OE8c4LyGb5AsE= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
 	'';
 }
