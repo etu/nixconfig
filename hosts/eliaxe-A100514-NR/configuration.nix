7514e7a3112437a632db3e92ad0b0723fc29abf4
Elis Hirwing <elis@hirwing.se>
2021-07-12 07:30:43 +0200

    hosts/eliaxe-A100514-NR: Set up remote builders and more binary caches

--- hosts/eliaxe-A100514-NR/configuration.nix
+++ hosts/eliaxe-A100514-NR/configuration.nix
@@ -137,4 +137,19 @@ in
   # Enable ClamAV.
   services.clamav.daemon.enable = true;
   services.clamav.updater.enable = true;
+
+  # Set up remote builds
+  nix.buildMachines = [{
+    hostName = "home.elis.nu";
+    system = "x86_64-linux";
+    maxJobs = 5;
+    sshUser = "root";
+    sshKey = "/persistent/home/root/.ssh/id_ed25519";
+  }];
+  nix.distributedBuilds = true;
+  nix.extraOptions = ''
+    builders-use-substitutes = true
+    substituters = https://cache.nixos.org https://cache.nixos.org/ https://fossar.cachix.org https://nix-community.cachix.org
+    trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= fossar.cachix.org-1:Zv6FuqIboeHPWQS7ysLCJ7UT7xExb4OE8c4LyGb5AsE= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+  '';
 }
