8d1e2d2d5acbb062505ee5ef55eeec2cc31d8307
Elis Hirwing <elis@hirwing.se>
2021-07-23 16:23:55 +0200

    deploy: Merge and clean deployments

--- deploy/private.nix
+++ deploy/private.nix
@@ -1,12 +1,20 @@
 let
   # Load niv sources
   sources = import ../nix/sources.nix;
-  nixusPath = "${sources.nixus}/default.nix";
+  nixus = import "${sources.nixus}/default.nix" { };
 in
-import nixusPath { } ({ config, ... }: {
+nixus ({ config, ... }: {
   # Set a nixpkgs version for all nodes
   defaults = { ... }: {
     nixpkgs = ../nix/nixos-unstable;
+  };
+
+  nodes.kodi = { lib, config, ... }: {
+    # How to reach this node
+    host = "root@192.168.0.105";
+
+    # What configuration it should have
+    configuration = ../hosts/kodi/configuration.nix;
   };
 
   nodes.fenchurch = { lib, config, ... }: {
