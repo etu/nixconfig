038fcae6ff963d06f79a8b1ab8a270055ca4247f
Elis Hirwing <elis@hirwing.se>
2021-07-23 22:29:35 +0200

    hosts/fenchurch: Drop some persistence files

--- hosts/fenchurch/persistence.nix
+++ hosts/fenchurch/persistence.nix
@@ -21,14 +21,9 @@
     ];
     extraDirectories = [
       ".dotfiles"
-      ".local/share/direnv"
-      ".local/share/emacs"
       ".local/share/fish"
       ".ssh"
       "backups"
-      "code"
-      "documents"
-      "org"
     ];
   };
 }
