c4644efca36853cb2f7b2c185a3d8eeedd3b4eea
Elis Hirwing <elis@hirwing.se>
2021-08-08 16:17:34 +0200

    shell: Install agenix cli util

--- shell.nix
+++ shell.nix
@@ -1,8 +1,14 @@
 { pkgs ? (import <nixpkgs> { }) }:
 
+let
+  # Load sources
+  agenix = pkgs.callPackage "${(import ./nix/sources.nix).agenix}/pkgs/agenix.nix" { };
+in
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    niv
-    nixpkgs-fmt
+  buildInputs = [
+    agenix
+
+    pkgs.niv
+    pkgs.nixpkgs-fmt
   ];
 }
