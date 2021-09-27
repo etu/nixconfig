3f519ed6b745cd6d11989a0cb6b783a2958f64b5
Elis Hirwing <elis@hirwing.se>
2021-09-27 16:58:30 +0200

    hosts/fenchurch: Make services really start after networking is up

--- hosts/fenchurch/raid-target.nix
+++ hosts/fenchurch/raid-target.nix
@@ -1,6 +1,14 @@
 { config, lib, ... }:
 
-{
+let
+  preStart = ''
+    echo "Checking for nameservers to appear in /etc/resolv.conf";
+    while ! grep nameserver /etc/resolv.conf > /dev/null; do
+      echo "Waiting for nameservers to appear in /etc/resolv.conf";
+      sleep 1;
+    done
+  '';
+in {
   config.systemd.targets.cryptraid = {
     wantedBy = [ "multi-user.target" ];
     after = [ "network-online.target" ];
@@ -24,4 +32,10 @@
     wantedBy = lib.mkForce [ "cryptraid.target" ];
     after = [ "media-legacy.mount" ];
   };
+
+  # Add a pre start check for network to be up for certain services.
+  config.systemd.services."podman-home-assistant".preStart = preStart;
+  config.systemd.services."container@freshrss".preStart = preStart;
+  config.systemd.services."container@jellyfin".preStart = preStart;
+  config.systemd.services."container@usenet".preStart = preStart;
 }
