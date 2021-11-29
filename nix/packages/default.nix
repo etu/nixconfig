139d27aed659c69cf70b77bb555903c5b0d92c2e
Elis Hirwing <elis@hirwing.se>
2021-11-29 20:26:33 +0100

    nix/packages: Extract the package version to use in the store path

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -1,8 +1,19 @@
-{ pkgs ? import <nixpkgs> { }, ... }:
+{ pkgs ? import <nixpkgs> { }, lib ? pkgs.lib, ... }:
 
 let
-  intelephensePkg = (pkgs.callPackage ./vscode-intelephense { }).package;
-  intelephense = pkgs.runCommandNoCC "intelephense-package" { } ''
+  intelephenseSources = (pkgs.callPackage ./vscode-intelephense { });
+  intelephensePkg = intelephenseSources.package;
+
+  # Extract version of the intelephense dependency used to have in the
+  # nix store path of the script.
+  intelephenseVersion = (lib.head (
+    builtins.attrValues (
+      lib.filterAttrs (n: v: v.packageName == "intelephense") intelephenseSources.sources
+    )
+  )).version;
+
+  # Build a wropper derivation.
+  intelephense = pkgs.runCommandNoCC "intelephense-package-${intelephenseVersion}" { } ''
     mkdir -p $out/bin
 
     ln -s ${intelephensePkg}/lib/node_modules/init-intelephense/node_modules/intelephense/lib/intelephense.js $out/bin/intelephense
