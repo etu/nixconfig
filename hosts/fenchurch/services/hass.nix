2ec4986f0164ff61a826f7e2591588406be7baba
Elis Hirwing <elis@hirwing.se>
2021-12-09 17:08:35 +0100

    Upgrade system

--- hosts/fenchurch/services/hass.nix
+++ hosts/fenchurch/services/hass.nix
@@ -45,7 +45,7 @@
       ];
     };
     zwavejs2mqtt = {
-      image = "zwavejs/zwavejs2mqtt:6.0.3";
+      image = "zwavejs/zwavejs2mqtt:6.1.0";
       ports = [
         "3000:3000"
         # "8091:8091" # Admin interface port
