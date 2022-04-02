9498be78eb7fd1f0fc738e2dfc77d48d6a30891b
Elis Hirwing <elis@hirwing.se>
2022-04-02 13:51:08 +0200

    feat(modules/home-manager): Move home-manager alacritty configs to sway file

--- modules/my-home-manager.nix
+++ modules/my-home-manager.nix
@@ -94,41 +94,6 @@ in
           '';
         };
 
-        programs.alacritty.enable = swayEnabled;
-        programs.alacritty.settings = {
-          env.TERM = "xterm-256color";
-          font.size = config.my.fonts.size;
-          font.normal.family = config.my.fonts.monospace;
-          bell = {
-            duration = 250;
-            color = "#441111";
-            animation = "EaseOut";
-          };
-          colors = {
-            primary = { background = "#000000"; foreground = "#dddddd"; };
-            normal = {
-              black = "#000000";
-              red = "#cc0403";
-              green = "#19cb00";
-              yellow = "#cecb00";
-              blue = "#0d73cc";
-              magenta = "#cb1ed1";
-              cyan = "#0dcdcd";
-              white = "#dddddd";
-            };
-            bright = {
-              black = "#767676";
-              red = "#f2201f";
-              green = "#23fd00";
-              yellow = "#fffd00";
-              blue = "#1a8fff";
-              magenta = "#fd28ff";
-              cyan = "#14ffff";
-              white = "#ffffff";
-            };
-          };
-        };
-
         programs.git = {
           enable = true;
 
