a48b226e4873b40c9f982ac65ad75097783cccb7
Elis Hirwing <elis@hirwing.se>
2022-05-15 09:22:18 +0200

    feat(treewide): Upgrade systems

--- hosts/fenchurch/services/hass.nix
+++ hosts/fenchurch/services/hass.nix
@@ -23,7 +23,7 @@
   virtualisation.oci-containers.containers = {
     home-assistant = {
       environment.TZ = config.time.timeZone;
-      image = "ghcr.io/home-assistant/home-assistant:2022.5.2";
+      image = "ghcr.io/home-assistant/home-assistant:2022.5.4";
       ports = [ "8123" ];
       extraOptions = [
         "--privileged"
@@ -45,7 +45,7 @@
       ];
     };
     zwavejs2mqtt = {
-      image = "zwavejs/zwavejs2mqtt:6.9.0";
+      image = "zwavejs/zwavejs2mqtt:6.9.1";
       ports = [
         "3000:3000"
         # "8091:8091" # Admin interface port
