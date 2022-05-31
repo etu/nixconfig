7539693ea05674fa8d42e0594b94e69edaf5f74f
Elis Hirwing <elis@hirwing.se>
2022-05-31 22:02:33 +0200

    fix(hosts/fenchurch): Improve jellyfin nginx proxy to follow upstream docs

--- hosts/fenchurch/services/jellyfin.nix
+++ hosts/fenchurch/services/jellyfin.nix
@@ -3,13 +3,30 @@
 {
   # Make sure to have nginx enabled
   services.nginx.enable = true;
+  services.nginx.clientMaxBodySize = "20m"; # Increase body size since we handle video.
   services.nginx.virtualHosts = {
-    "jellyfin.elis.nu" = {
+    "jellyfin.elis.nu" = let
+      locationConfig = {
+        proxyWebsockets = true;
+        proxyPass = "http://127.0.0.1:8096/";
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header X-Forwarded-Protocol $scheme;
+          proxy_set_header X-Forwarded-Host $http_host;
+
+          # Disable buffering when the nginx proxy gets very resource heavy upon streaming
+          proxy_buffering off;
+      '';
+      };
+    in {
       forceSSL = true;
       enableACME = true;
-      locations."/".proxyWebsockets = true;
-      locations."/".proxyPass = "http://127.0.0.1:8096/";
-      locations."/".extraConfig = "proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;";
+      locations."/" = locationConfig;
+      locations."= /web/" = locationConfig;
+      locations."/socket" = locationConfig;
     };
   };
 
