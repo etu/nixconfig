89a7533ac50e5ba0e224b5fba7b19df1d7c08a71
Elis Hirwing <elis@hirwing.se>
2022-07-01 13:00:18 +0200

    feat(hosts/fenchurch): Enable hardware acceleration for transcode in jellyfin

--- hosts/fenchurch/services/jellyfin.nix
+++ hosts/fenchurch/services/jellyfin.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Make sure to have nginx enabled
@@ -36,6 +36,27 @@
     options = [ "bind" "noauto" "x-systemd.automount" ];
     noCheck = true;
   };
+
+  # Enable vaapi on OS-level
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
+  # Allow jellyfin user to access the graphics card
+  users.users.${config.services.jellyfin.user}.extraGroups = [ "video" "render" ];
+
+  # Override default hardening measure from NixOS
+  systemd.services.jellyfin.serviceConfig.PrivateDevices = lib.mkForce false;
+  systemd.services.jellyfin.serviceConfig.DeviceAllow = lib.mkForce [ "/dev/dri/renderD128" ];
 
   # Enable jellyfin itself
   services.jellyfin.enable = true;
