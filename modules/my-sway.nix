8785a7c607bd61d6d46aa5965958ce7420880987
Elis Hirwing <elis@hirwing.se>
2022-07-07 12:05:03 +0200

    feat(modules): Drop redundant enabling of browserpass

--- modules/my-sway.nix
+++ modules/my-sway.nix
@@ -7,8 +7,6 @@ in
   config = lib.mkIf cfg.enable {
     # Home manager settings for sway and programs related to sway
     home-manager.users.${config.etu.user.username} = lib.mkIf cfg.enable {
-      programs.browserpass.enable = cfg.enable;
-
       xdg.mimeApps = {
         enable = cfg.enable;
         defaultApplications = {
