a45944fe1960070c21ac18b9a722d32f5412e40e
Elis Hirwing <elis@hirwing.se>
2022-07-12 11:51:03 +0200

    feat(treewide): Upgrade systems

--- hosts/fenchurch/services/hass.nix
+++ hosts/fenchurch/services/hass.nix
@@ -23,7 +23,7 @@
   virtualisation.oci-containers.containers = {
     home-assistant = {
       environment.TZ = config.time.timeZone;
-      image = "ghcr.io/home-assistant/home-assistant:2022.7.1";
+      image = "ghcr.io/home-assistant/home-assistant:2022.7.3";
       ports = [ "8123" ];
       extraOptions = [
         "--privileged"
