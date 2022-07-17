adf39cacdd1807c665b0aab79fb4ac3aac465600
Elis Hirwing <elis@hirwing.se>
2022-07-17 08:48:08 +0200

    feat(dotfiles): Move additional configurations into nix

--- modules/graphical/gnupg/default.nix
+++ modules/graphical/gnupg/default.nix
@@ -22,20 +22,44 @@
         # Install pass with expensions
         (pkgs.pass.withExtensions (ext: with ext; [ pass-otp pass-update pass-checkup ]))
 
-        # Keysigning party
+        # Signing party for the tool caff
         pkgs.signing-party
-        pkgs.msmtp
       ];
+
+      home.file = {
+        ".caffrc".text = ''
+          # .caffrc -- vim:ft=perl:
+          # This file is in perl(1) format - see caff(1) for details.
+
+          $CONFIG{'owner'} = '${config.etu.user.realname}';
+          $CONFIG{'email'} = '${config.etu.user.email}';
+
+          # I used to have the last 16 chars of my key's fingerprint
+          # here but have replaced it with the full fingerprint. This
+          # is a note to the future if this is a problem.
+          $CONFIG{'keyid'} = [ qw{${config.etu.user.signingKey}} ];
+
+          $ENV{'PERL_MAILERS'} = 'sendmail:${pkgs.msmtp}/bin/msmtp';
+        '';
+        ".msmtprc".text = ''
+          account default
+          host smtp.gmail.com
+          tls on
+          tls_certcheck on
+          tls_trust_file /etc/ssl/certs/ca-certificates.crt
+          auth on
+          user ${config.etu.user.email}
+          from ${config.etu.user.email}
+          passwordeval "pass authinfo | grep -e 'smtp ${config.etu.user.email}' | cut -d ' ' -f 3"
+          port 587
+        '';
+      };
     };
 
     # Enable persistence for gnupg and pass files.
     etu.base.zfs.user.directories = [
       ".gnupg"
       ".password-store"
-    ];
-    etu.base.zfs.user.files = [
-      ".caffrc"
-      ".msmtprc"
     ];
   };
 }
