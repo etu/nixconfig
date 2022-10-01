2820cb8ef2a36af950bc4311f480b49bed40a736
Elis Hirwing <elis@hirwing.se>
2022-10-01 16:56:49 +0200

    feat(treewide): Upgrade systems

--- hosts/home-server/services/hass.nix
+++ hosts/home-server/services/hass.nix
@@ -48,7 +48,7 @@
       ];
     };
     zwavejs2mqtt = {
-      image = "zwavejs/zwavejs2mqtt:8.0.1";
+      image = "zwavejs/zwavejs2mqtt:8.0.2";
       ports = [
         "3000:3000"
         # "8091:8091" # Admin interface port
