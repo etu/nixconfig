8d58e58f2206faf57d04565e2be936b4e00d2d7e
Elis Hirwing <elis@hirwing.se>
2022-10-27 09:43:17 +0200

    feat(modules/work): Install misc utils

--- modules/work/default.nix
+++ modules/work/default.nix
@@ -18,6 +18,8 @@ in {
     # Allow certain unfree packages.
     etu.base.nix.allowUnfree = [
       "appgate-sdp"
+      "google-chrome"
+      "slack"
     ];
 
     etu.user.extraUserPackages = [
@@ -26,6 +28,25 @@ in {
 
       # Install make
       pkgs.gnumake
+
+      # Install chef and vagrant for some legacy systems reasons
+      pkgs.chefdk
+      pkgs.vagrant
+
+      # Kubernetes and Docker utils
+      pkgs.docker-compose
+      pkgs.kubectl
+      pkgs.kubectx
+      pkgs.kubetail
+      pkgs.kubent
+      pkgs.kubernetes-helm
+      pkgs.minikube
+      pkgs.octant
+
+      # Misc
+      pkgs.google-chrome
+      pkgs.mysql57
+      pkgs.slack
     ];
 
     # Enable appgate
