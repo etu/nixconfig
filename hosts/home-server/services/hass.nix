5f68b0466f2916d962bf776c08ca8f624713154b
etu <etu@users.noreply.github.com>
2022-11-26 04:03:43 +0000

    cron(treewide): Upgrade systems

--- hosts/home-server/services/hass.nix
+++ hosts/home-server/services/hass.nix
@@ -46,7 +46,7 @@
       ];
     };
     zwavejs2mqtt = {
-      image = "zwavejs/zwavejs2mqtt:8.5.0";
+      image = "zwavejs/zwavejs2mqtt:8.5.1";
       ports = [
         "3000:3000"
         # "8091:8091" # Admin interface port
