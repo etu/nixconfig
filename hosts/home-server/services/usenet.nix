2a9366de072fbfa520ceac552e58b1f0bec5f3bc
Elis Hirwing <elis@hirwing.se>
2022-11-30 22:37:05 +0100

    fix(hosts/home-server/nginx): Re-organize the nix code for some locations

--- hosts/home-server/services/usenet.nix
+++ hosts/home-server/services/usenet.nix
@@ -3,68 +3,61 @@
 {
   # Make sure to have nginx enabled
   services.nginx.enable = true;
-  services.nginx.virtualHosts =
-    let
-      onlyLan = ''
-        allow 192.168.1.0/24;
-        allow 127.0.0.1/24;
-        deny all;
+  services.nginx.virtualHosts."local.elis.nu".locations = let
+    onlyLan = ''
+      allow 192.168.1.0/24;
+      allow 127.0.0.1/24;
+      deny all;
+    '';
+  in {
+    # Index file
+    "/" = {
+      root = pkgs.writeTextDir "index.html" ''
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8" />
+            <title>Links</title>
+          </head>
+          <body>
+            <ul>
+              <li><a href="/bazarr">Bazarr (Subtitles)</a></li>
+              <li><a href="/sonarr">Sonarr (Series)</a></li>
+              <li><a href="/radarr">Radarr (Movies)</a></li>
+              <li><a href="/lidarr">Lidarr (Music)</a></li>
+              <li><a href="/nzbget">NzbGet</a></li>
+            </ul>
+          </body>
+        </html>
       '';
-    in
-    {
-      # Index file
-      "local.elis.nu".locations."/" = {
-        root = pkgs.writeTextDir "index.html" ''
-          <!DOCTYPE html>
-          <html lang="en">
-            <head>
-              <meta charset="utf-8" />
-              <title>Links</title>
-            </head>
-            <body>
-              <ul>
-                <li><a href="/bazarr">Bazarr (Subtitles)</a></li>
-                <li><a href="/sonarr">Sonarr (Series)</a></li>
-                <li><a href="/radarr">Radarr (Movies)</a></li>
-                <li><a href="/lidarr">Lidarr (Music)</a></li>
-                <li><a href="/nzbget">NzbGet</a></li>
-              </ul>
-            </body>
-          </html>
-        '';
-        extraConfig = onlyLan;
-      };
-      # Bazarr
-      "local.elis.nu".locations."/bazarr" = {
-        proxyPass = "http://127.0.0.1:6767/bazarr";
-        extraConfig = onlyLan;
-      };
-      # Sonarr
-      "local.elis.nu".locations."/sonarr" = {
-        proxyPass = "http://127.0.0.1:8989/sonarr";
-        extraConfig = onlyLan;
-      };
-      # Radarr
-      "local.elis.nu".locations."/radarr" = {
-        proxyPass = "http://127.0.0.1:7878/radarr";
-        extraConfig = onlyLan;
-      };
-      # Lidarr
-      "local.elis.nu".locations."/lidarr" = {
-        proxyPass = "http://127.0.0.1:8686/lidarr";
-        extraConfig = onlyLan;
-      };
-      # Nzbget
-      "local.elis.nu".locations."~ ^/nzbget($|./*)" = {
-        proxyPass = "http://127.0.0.1:6789";
-        extraConfig = onlyLan + ''
-          rewrite /nzbget/(.*) /$1 break;
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        '';
-      };
+      extraConfig = onlyLan;
     };
+    "/bazarr" = {
+      proxyPass = "http://127.0.0.1:6767/bazarr";
+      extraConfig = onlyLan;
+    };
+    "/sonarr" = {
+      proxyPass = "http://127.0.0.1:8989/sonarr";
+      extraConfig = onlyLan;
+    };
+    "/radarr" = {
+      proxyPass = "http://127.0.0.1:7878/radarr";
+      extraConfig = onlyLan;
+    };
+    "/lidarr" = {
+      proxyPass = "http://127.0.0.1:8686/lidarr";
+      extraConfig = onlyLan;
+    };
+    "~ ^/nzbget($|./*)" = {
+      proxyPass = "http://127.0.0.1:6789";
+      extraConfig = onlyLan + ''
+        rewrite /nzbget/(.*) /$1 break;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      '';
+    };
+  };
 
   # Enable usenet related services in a container
   containers.usenet = {
