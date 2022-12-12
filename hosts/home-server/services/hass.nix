5f1f2819085ba777880bf42b2851c991312c097d
etu <etu@users.noreply.github.com>
2022-12-12 09:07:34 +0000

    cron(treewide): Upgrade systems

--- hosts/home-server/services/hass.nix
+++ hosts/home-server/services/hass.nix
@@ -22,7 +22,7 @@
   virtualisation.oci-containers.containers = {
     home-assistant = {
       environment.TZ = config.time.timeZone;
-      image = "ghcr.io/home-assistant/home-assistant:2022.12.1";
+      image = "ghcr.io/home-assistant/home-assistant:2022.12.3";
       ports = [ "8123" ];
       extraOptions = [
         "--network=host"
