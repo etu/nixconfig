3a8719be6af411c16ed58de693013690c1615059
Elis Hirwing <elis@hirwing.se>
2023-02-25 08:11:14 +0100

    chore(treewide): Upgrade systems manually

--- hosts/server-main-elis/services/hass.nix
+++ hosts/server-main-elis/services/hass.nix
@@ -48,7 +48,7 @@
       ];
     };
     zwavejs2mqtt = {
-      image = "zwavejs/zwavejs2mqtt:8.8.6";
+      image = "zwavejs/zwavejs2mqtt:8.9.0";
       ports = [
         "3000:3000"
         # "8091:8091" # Admin interface port
