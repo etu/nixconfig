56fe4a9fc6e679ad5843b4004df7a03fe2750405
Elis Hirwing <elis@hirwing.se>
2023-08-30 21:37:30 +0200

    feat(modules/gnupg): Uninstall caff and signing party related programs and configs

--- modules/graphical/gnupg/default.nix
+++ modules/graphical/gnupg/default.nix
@@ -17,37 +17,6 @@
       pinentryFlavor = "gnome3";
     };
 
-    home-manager.users.${config.etu.user.username} = lib.mkIf config.etu.user.enable {
-      home.file = {
-        ".caffrc".text = ''
-          # .caffrc -- vim:ft=perl:
-          # This file is in perl(1) format - see caff(1) for details.
-
-          $CONFIG{'owner'} = '${config.etu.user.realname}';
-          $CONFIG{'email'} = '${config.etu.user.email}';
-
-          # I used to have the last 16 chars of my key's fingerprint
-          # here but have replaced it with the full fingerprint. This
-          # is a note to the future if this is a problem.
-          $CONFIG{'keyid'} = [ qw{${config.etu.user.signingKey}} ];
-
-          $ENV{'PERL_MAILERS'} = 'sendmail:${pkgs.msmtp}/bin/msmtp';
-        '';
-        ".msmtprc".text = ''
-          account default
-          host smtp.gmail.com
-          tls on
-          tls_certcheck on
-          tls_trust_file /etc/ssl/certs/ca-certificates.crt
-          auth on
-          user ${config.etu.user.email}
-          from ${config.etu.user.email}
-          passwordeval "pass authinfo | grep -e 'smtp ${config.etu.user.email}' | cut -d ' ' -f 3"
-          port 587
-        '';
-      };
-    };
-
     # Install tools using home manager.
     etu.user.extraUserPackages = [
       # Install gnupg
@@ -55,9 +24,6 @@
 
       # Install pass with expensions
       (pkgs.pass.withExtensions (ext: with ext; [pass-otp pass-update pass-checkup]))
-
-      # Signing party for the tool caff
-      pkgs.signing-party
     ];
 
     # Enable persistence for gnupg and pass files.
