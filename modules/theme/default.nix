ca13c7d588378685627056c3b48df08b713b456a
Elis Hirwing <elis@hirwing.se>
2024-08-21 20:43:35 +0200

    feat(modules/theme): Enable catppuccin system module

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -7,6 +7,9 @@
   options.etu.theme.enable = lib.mkEnableOption "Enable theme settings";
 
   config = lib.mkIf config.etu.theme.enable {
+    # Globally enable catppuccin themes on system level.
+    catppuccin.enable = true;
+
     # Set up themes for home manager for the main user.
     home-manager.users.${config.etu.user.username} = lib.mkIf config.etu.user.enable {
       # Import the catppuccin home manager modules.
