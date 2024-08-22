431edeb4612faaf4d702fc41a3601b0efab96259
Elis Hirwing <elis@hirwing.se>
2024-08-22 21:02:17 +0200

    feat(flake): Add a package with a mix of space backgrounds

--- flake.nix
+++ flake.nix
@@ -117,7 +117,7 @@
             inherit (inputs) catppuccin;
             inherit (pkgs-22-11.nodejs-14_x.pkgs) intelephense;
             inherit (pkgs-22-11) chefdk;
-            inherit (self.packages.${system}) swayWallpaper;
+            inherit (self.packages.${system}) swayWallpaper spaceWallpapers;
             inherit myData;
 
             emacs-overlay = inputs.emacs-overlay.overlay;
@@ -214,6 +214,7 @@
 
       # Build packages
       packages.swayWallpaper = pkgs.callPackage ./packages/wallpaper {};
+      packages.spaceWallpapers = pkgs.callPackage ./packages/spaceWallpapers {};
       packages.iso = self.nixosConfigurations.live-iso.config.system.build.isoImage;
     });
 }
