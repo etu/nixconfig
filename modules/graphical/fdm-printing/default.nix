6350bb7c074b58d6bdd868df1e4f1965177fce77
Elis Hirwing <elis@hirwing.se>
2024-10-13 10:57:27 +0200

    fix(modules/fdm-printing): Install FreeCAD

--- modules/graphical/fdm-printing/default.nix
+++ modules/graphical/fdm-printing/default.nix
@@ -9,11 +9,13 @@
   config = lib.mkIf config.etu.graphical.fdm-printing.enable {
     # Install using home-manager.
     etu.user.extraUserPackages = [
+      pkgs.freecad-wayland
       pkgs.openscad-unstable
     ];
 
     # Enable persistence for fdm-printing related files.
     etu.base.zfs.user.directories = [
+      ".config/FreeCAD"
       ".config/OpenSCAD"
     ];
   };
