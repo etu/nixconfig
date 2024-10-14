2188dbc14d9b1ae6571e7206c2b36c780de39494
Elis Hirwing <elis@hirwing.se>
2024-10-14 09:45:19 +0200

    feat(hosts/server-main): Upgrade nextcloud from 29 to 30

--- hosts/server-main-elis/services/nextcloud.nix
+++ hosts/server-main-elis/services/nextcloud.nix
@@ -5,11 +5,11 @@
   ...
 }: {
   services.nextcloud.enable = true;
-  services.nextcloud.package = pkgs.nextcloud29;
+  services.nextcloud.package = pkgs.nextcloud30;
   services.nextcloud.config.adminuser = "etu";
   services.nextcloud.config.adminpassFile = config.age.secrets.nextcloud-admin-password.path;
   services.nextcloud.extraApps = {
-    inherit (pkgs.nextcloud29Packages.apps) cookbook notes;
+    inherit (pkgs.nextcloud30Packages.apps) cookbook notes;
   };
   services.nextcloud.hostName = "nextcloud.elis.nu";
   services.nextcloud.https = true;
