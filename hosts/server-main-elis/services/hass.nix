a570cec5f5445db2456e3c8990a23971074b0f7f
etu <etu@users.noreply.github.com>
2024-11-22 06:54:34 +0000

    cron(treewide): Upgrade systems

--- hosts/server-main-elis/services/hass.nix
+++ hosts/server-main-elis/services/hass.nix
@@ -38,7 +38,7 @@
   virtualisation.oci-containers.containers = {
     home-assistant = {
       environment.TZ = config.time.timeZone;
-      image = "ghcr.io/home-assistant/home-assistant:2024.11.1";
+      image = "ghcr.io/home-assistant/home-assistant:2024.11.2";
       ports = ["8123"];
       extraOptions = [
         "--network=host"
@@ -64,7 +64,7 @@
       ];
     };
     zwavejs2mqtt = {
-      image = "zwavejs/zwavejs2mqtt:9.26.0";
+      image = "zwavejs/zwavejs2mqtt:9.27.4";
       ports = [
         "3000:3000"
         # "8091:8091" # Admin interface port
