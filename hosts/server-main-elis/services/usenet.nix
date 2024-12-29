b9f00d8f2f2282f1dfb7b00d0b77aba68443c367
Elis Hirwing <elis@hirwing.se>
2024-12-29 13:32:12 +0100

    feat(hosts/server-main-elis): Drop the systemd container

--- hosts/server-main-elis/services/usenet.nix
+++ hosts/server-main-elis/services/usenet.nix
@@ -43,100 +43,50 @@
     };
   };
 
-  # Enable usenet related services in a container
-  containers.usenet = {
-    autoStart = true;
-    config = {lib, ...}: {
-      # The NixOS release to be compatible with for stateful data such as databases.
-      system.stateVersion = config.etu.stateVersion;
+  # Persistent directory mounts
+  etu.base.zfs.system.directories = [
+    "/var/lib/bazarr"
+    "/var/lib/lidarr"
+    "/var/lib/nzbget"
+    "/var/lib/radarr"
+    "/var/lib/sonarr"
+  ];
 
-      # dotnet for sonarr seems to be very old
-      nixpkgs.config.permittedInsecurePackages = [
-        "aspnetcore-runtime-6.0.36"
-        "aspnetcore-runtime-wrapped-6.0.36"
-        "dotnet-sdk-6.0.428"
-        "dotnet-sdk-wrapped-6.0.428"
-      ];
+  # nzbget needs unrar
+  etu.base.nix.allowUnfree = ["unrar"];
 
-      # Disable documentation to make the system smaller.
-      documentation.enable = false;
-      documentation.doc.enable = false;
-      documentation.info.enable = false;
-      documentation.man.enable = false;
+  # dotnet for sonarr seems to be very old
+  nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-6.0.36"
+    "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-6.0.428"
+    "dotnet-sdk-wrapped-6.0.428"
+  ];
 
-      # nzbget needs unrar
-      nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) ["unrar"];
-
-      services.bazarr = {
-        enable = true;
-        user = "downloads";
-        group = "downloads";
-      };
-      services.sonarr = {
-        enable = true;
-        user = "downloads";
-        group = "downloads";
-      };
-      services.radarr = {
-        enable = true;
-        user = "downloads";
-        group = "downloads";
-      };
-      services.lidarr = {
-        enable = true;
-        user = "downloads";
-        group = "downloads";
-      };
-      services.nzbget = {
-        enable = true;
-        user = "downloads";
-        group = "downloads";
-      };
-
-      users.users.downloads = {
-        group = "downloads";
-        uid = 947;
-        isSystemUser = true;
-      };
-      users.groups.downloads.gid = 947;
-    };
-
-    bindMounts = {
-      bazarr = {
-        mountPoint = "/var/lib/bazarr";
-        hostPath = "${config.etu.dataPrefix}/var/lib/bazarr";
-        isReadOnly = false;
-      };
-      sonarr = {
-        mountPoint = "/var/lib/sonarr/.config/NzbDrone";
-        hostPath = "${config.etu.dataPrefix}/var/lib/sonarr";
-        isReadOnly = false;
-      };
-      radarr = {
-        mountPoint = "/var/lib/radarr/.config/Radarr";
-        hostPath = "${config.etu.dataPrefix}/var/lib/radarr";
-        isReadOnly = false;
-      };
-      lidarr = {
-        mountPoint = "/var/lib/lidarr/.config/Lidarr";
-        hostPath = "${config.etu.dataPrefix}/var/lib/lidarr";
-        isReadOnly = false;
-      };
-      nzbget = {
-        mountPoint = "/var/lib/nzbget";
-        hostPath = "${config.etu.dataPrefix}/var/lib/nzbget";
-        isReadOnly = false;
-      };
-      nzbget-dst = {
-        mountPoint = "/var/lib/nzbget-dst";
-        hostPath = "/var/lib/nzbget-dst";
-        isReadOnly = false;
-      };
-      media = {
-        mountPoint = "/media";
-        hostPath = "/media/zstorage/files";
-        isReadOnly = false;
-      };
-    };
+  # Enable services
+  services.bazarr = {
+    enable = true;
+    user = "downloads";
+    group = "downloads";
+  };
+  services.nzbget = {
+    enable = true;
+    user = "downloads";
+    group = "downloads";
+  };
+  services.sonarr = {
+    enable = true;
+    user = "downloads";
+    group = "downloads";
+  };
+  services.radarr = {
+    enable = true;
+    user = "downloads";
+    group = "downloads";
+  };
+  services.lidarr = {
+    enable = true;
+    user = "downloads";
+    group = "downloads";
   };
 }
