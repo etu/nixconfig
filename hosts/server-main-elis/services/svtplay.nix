f517ef9055295155e2a418c1494d2265f8909035
Elis Hirwing <elis@hirwing.se>
2024-12-30 12:36:15 +0100

    fix(hosts/server-main-elis): Fix SVTPlay timers

--- hosts/server-main-elis/services/svtplay.nix
+++ hosts/server-main-elis/services/svtplay.nix
@@ -1,21 +1,40 @@
-_: {
-  #buildSvtPlayService = svtSlug: {
-  #  description = "${svtSlug} updater";
-  #  after = ["network-online.target"];
-  #  wantedBy = ["multi-user.target"];
-  #  startAt = "04:00";
-  #  path = [pkgs.ffmpeg];
-  #  preStart = "mkdir -p ${svtSlug}";
-  #  serviceConfig = {
-  #    Type = "oneshot";
-  #    User = "downloads";
-  #    Group = "downloads";
-  #    WorkingDirectory = "/media/zstorage/files/video/svt-series";
-  #    ExecStart = "${pkgs.svtplay-dl}/bin/svtplay-dl --nfo --all-episodes --output ${svtSlug} https://www.svtplay.se/${svtSlug}";
-  #  };
-  #};
+{pkgs, ...}: let
+  buildSvtPlayService = svtSlug: {
+    description = "${svtSlug} updater";
+    wants = ["network-online.target"];
+    after = ["network-online.target"];
+    path = [pkgs.ffmpeg pkgs.svtplay-dl];
+    script = "svtplay-dl --nfo --all-episodes --output ${svtSlug} https://www.svtplay.se/${svtSlug}";
+    serviceConfig = {
+      Type = "simple";
+      User = "downloads";
+      Group = "downloads";
+      WorkingDirectory = "/media/zstorage/files/video/svt-series";
+    };
+  };
+  buildSvtPlayTimer = svtSlug: {
+    description = "${svtSlug} updater timer";
+    after = ["network-online.target"];
+    wants = ["network-online.target"];
+    wantedBy = ["timers.target"];
+    timerConfig.OnCalendar = "daily";
+  };
+in {
+  systemd.services.svtplay-faret-shaun = buildSvtPlayService "faret-shaun";
+  systemd.timers.svtplay-faret-shaun = buildSvtPlayTimer "faret-shaun";
 
-  #systemd.services.svtplay-faret-shaun = buildSvtPlayService "faret-shaun";
-  #systemd.services.svtplay-mamma-mu = buildSvtPlayService "mamma-mu";
-  #systemd.services.svtplay-pettson-och-findus = buildSvtPlayService "pettson-och-findus";
+  systemd.services.svtplay-mamma-mu = buildSvtPlayService "mamma-mu";
+  systemd.timers.svtplay-mamma-mu = buildSvtPlayTimer "mamma-mu";
+
+  systemd.services.svtplay-pettson-och-findus = buildSvtPlayService "pettson-och-findus";
+  systemd.timers.svtplay-pettson-och-findus = buildSvtPlayTimer "pettson-och-findus";
+
+  systemd.services.svtplay-filtis-och-fluff = buildSvtPlayService "filtis-och-fluff";
+  systemd.timers.svtplay-filtis-och-fluff = buildSvtPlayTimer "filtis-och-fluff";
+
+  systemd.services.svtplay-greta-gris = buildSvtPlayService "greta-gris";
+  systemd.timers.svtplay-greta-gris = buildSvtPlayTimer "greta-gris";
+
+  systemd.services.svtplay-minibods = buildSvtPlayService "minibods";
+  systemd.timers.svtplay-minibods = buildSvtPlayTimer "minibods";
 }
