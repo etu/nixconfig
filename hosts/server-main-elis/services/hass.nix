7913565cbfea34a70757c9079715b8e3bdf6d6fb
Elis Hirwing <elis@hirwing.se>
2025-03-06 08:34:10 +0100

    fix(hosts/server-main-elis): Upgrade home assistant

--- hosts/server-main-elis/services/hass.nix
+++ hosts/server-main-elis/services/hass.nix
@@ -20,7 +20,7 @@
   virtualisation.oci-containers.containers = {
     home-assistant = {
       environment.TZ = config.time.timeZone;
-      image = "ghcr.io/home-assistant/home-assistant:2025.2.5";
+      image = "ghcr.io/home-assistant/home-assistant:2025.3.0";
       ports = ["8123"];
       extraOptions = [
         "--network=host"
