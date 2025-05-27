96526f52f8af7f75ba5359f5889ca1e122ac4658
Elis Hirwing <elis@hirwing.se>
2025-05-27 08:10:35 +0200

    fix(modules/vscode): Fix copilot-chat extension for vscode update

--- modules/development/vscode/default.nix
+++ modules/development/vscode/default.nix
@@ -57,8 +57,8 @@
       # This is to pin it to a version to be compatible with vscode.
       publisher = "github";
       name = "copilot-chat";
-      version = "0.26.2025040204";
-      sha256 = "sha256-grG/pn+R4paCqkSx6DGzKjyjQVZ2FINRxdpXynGF35g=";
+      version = "0.27.2025050603";
+      sha256 = "sha256-OuwTC6blGMhm2Ib9hnAybUuooslDLUQWRyBPVaI37hs=";
     };
   };
 in {
