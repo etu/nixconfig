4aa9171189aa4e703cb91a4906490a7b2ad66f8c
Elis Hirwing <elis@hirwing.se>
2025-08-11 07:18:42 +0200

    fix(hosts/server-main-elis): Update zwavejs2mqtt

--- hosts/server-main-elis/services/hass.nix
+++ hosts/server-main-elis/services/hass.nix
@@ -46,7 +46,7 @@
       ];
     };
     zwavejs2mqtt = {
-      image = "zwavejs/zwavejs2mqtt:10.1.0";
+      image = "zwavejs/zwavejs2mqtt:11.1.0";
       ports = [
         "3000:3000"
         # "8091:8091" # Admin interface port
