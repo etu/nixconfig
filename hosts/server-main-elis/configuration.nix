4f1e07a83d1acd102dcbe3660e7391b255d509a0
Elis Hirwing <elis@hirwing.se>
2025-09-07 19:48:08 +0200

    feat(hosts/server-main-elis): Drop guest ssh user

--- hosts/server-main-elis/configuration.nix
+++ hosts/server-main-elis/configuration.nix
@@ -17,7 +17,6 @@
     # Import local services that are host specific
     ./services/cfdyndns.nix
     ./services/empty-dirs-cleaner.nix
-    ./services/guest-users.nix
     ./services/hass.nix
     ./services/home-nginx.nix
     ./services/homepage.nix
