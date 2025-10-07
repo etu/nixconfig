b677c451d58ee866b09e20a7c712e85a2a2839fc
Elis Hirwing <elis@hirwing.se>
2025-10-07 22:03:09 +0200

    feat(modules): Migrate htop configs to be a home manager module

--- modules/nixos/base/htop/default.nix
+++ modules/nixos/base/htop/default.nix
@@ -1,54 +1,10 @@
 {
   config,
+  flake,
   lib,
   pkgs,
   ...
 }:
-let
-  base = {
-    programs.htop = {
-      enable = true;
-      settings = {
-        hide_userland_threads = true;
-        hide_kernel_threads = true;
-        highlight_base_name = true;
-        shadow_other_users = true;
-        show_program_path = false;
-        tree_view = true;
-
-        left_meters = [
-          "LeftCPUs"
-          "Memory"
-          "Swap"
-          "ZFSARC"
-          "ZFSCARC"
-        ];
-        left_meter_modes = [
-          1
-          1
-          1
-          2
-          2
-        ];
-
-        right_meters = [
-          "RightCPUs"
-          "Tasks"
-          "LoadAverage"
-          "Uptime"
-          "Battery"
-        ];
-        right_meter_modes = [
-          1
-          2
-          2
-          2
-          2
-        ];
-      };
-    };
-  };
-in
 {
   options.etu.base.htop.enable = lib.mkEnableOption "Enable base htop settings";
 
@@ -57,9 +13,15 @@ in
     environment.systemPackages = [ pkgs.htop ];
 
     # Configure htop for my users home-manager (if it's enabled).
-    home-manager.users.${config.etu.user.username} = lib.mkIf config.etu.user.enable base;
+    home-manager.users.${config.etu.user.username} = lib.mkIf config.etu.user.enable {
+      imports = [
+        flake.homeModules.htop
+      ];
+    };
 
     # Configure htop for root users home-manager.
-    home-manager.users.root = base;
+    home-manager.users.root.imports = [
+      flake.homeModules.htop
+    ];
   };
 }
