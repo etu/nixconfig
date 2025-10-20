5629567039e7fed8d50896b8ac17bbf80cf04a86
Elis Hirwing <elis@hirwing.se>
2025-10-20 19:08:09 +0200

    fix(hosts/server-main-eils): Upgrade from nextcloud 31 to nextcloud 32

--- hosts/server-main-elis/services/nextcloud.nix
+++ hosts/server-main-elis/services/nextcloud.nix
@@ -5,12 +5,12 @@
 }:
 {
   services.nextcloud.enable = true;
-  services.nextcloud.package = pkgs.nextcloud31;
+  services.nextcloud.package = pkgs.nextcloud32;
   services.nextcloud.config.adminuser = "etu";
   services.nextcloud.config.adminpassFile = config.age.secrets.nextcloud-admin-password.path;
   services.nextcloud.config.dbtype = "sqlite";
   services.nextcloud.extraApps = {
-    inherit (pkgs.nextcloud31Packages.apps) cookbook notes;
+    inherit (pkgs.nextcloud32Packages.apps) cookbook notes;
   };
   services.nextcloud.hostName = "nextcloud.elis.nu";
   services.nginx.enable = true;
