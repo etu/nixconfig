b3361cf6a4b1a649a3a922ca61e90c551f7cc860
Elis Hirwing <elis@hirwing.se>
2025-11-11 07:25:59 +0100

    fix(modules/jellyfin): Fix for vaapiIntel package rename to intel-vaapi-driver

--- modules/nixos/services/jellyfin/default.nix
+++ modules/nixos/services/jellyfin/default.nix
@@ -22,13 +22,13 @@
 
     # Enable vaapi on OS-level
     nixpkgs.config.packageOverrides = pkgs: {
-      vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+      intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
     };
     hardware.graphics = {
       enable = true;
       extraPackages = [
         pkgs.intel-media-driver
-        pkgs.vaapiIntel
+        pkgs.intel-vaapi-driver
         pkgs.vaapiVdpau
         pkgs.libvdpau-va-gl
       ];
