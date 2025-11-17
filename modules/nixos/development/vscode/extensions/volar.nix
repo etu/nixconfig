9589a3159cb1a58faa3d410cafd7b533a3a45341
etu <461970+etu@users.noreply.github.com>
2025-11-17 03:50:28 +0000

    cron(treewide): Upgrade systems

--- modules/nixos/development/vscode/extensions/volar.nix
+++ modules/nixos/development/vscode/extensions/volar.nix
@@ -6,6 +6,6 @@
 pkgs.vscode-utils.extensionFromVscodeMarketplace {
   publisher = "Vue";
   name = "volar";
-  version = "3.1.1";
-  sha256 = "sha256-l5x7fLznJGm2dPAbJlz4/5BDikM45h1W9GkmoC4Sv7k=";
+  version = "3.1.4";
+  sha256 = "sha256-BjGgrt2XPiswOoTHPdWALQOXBXLxtXvTdxJTNlKAkqU=";
 }
