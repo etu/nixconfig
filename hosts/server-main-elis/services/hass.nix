16c0ffcb812893ff100529e5216357e81d087e2f
Elis Hirwing <elis@hirwing.se>
2026-01-16 08:07:45 +0100

    chore(treewide): Upgrade systems

--- hosts/server-main-elis/services/hass.nix
+++ hosts/server-main-elis/services/hass.nix
@@ -10,7 +10,7 @@
   virtualisation.oci-containers.containers = {
     home-assistant = {
       environment.TZ = config.time.timeZone;
-      image = "ghcr.io/home-assistant/home-assistant:2026.1.0";
+      image = "ghcr.io/home-assistant/home-assistant:2026.1.1";
       ports = [ "8123" ];
       extraOptions = [
         "--network=host"
@@ -39,7 +39,7 @@
       ];
     };
     zwavejs2mqtt = {
-      image = "zwavejs/zwavejs2mqtt:11.9.1";
+      image = "zwavejs/zwavejs2mqtt:11.10.1";
       ports = [
         "3000:3000"
         # "8091:8091" # Admin interface port
