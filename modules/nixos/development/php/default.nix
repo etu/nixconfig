e9c96bb0d3b71e64cdeebfb33b771a731d8b9d8c
Elis Hirwing <elis@hirwing.se>
2026-01-21 12:49:37 +0100

    fix(modules/php): Persistence for composer files

--- modules/nixos/development/php/default.nix
+++ modules/nixos/development/php/default.nix
@@ -8,6 +8,11 @@
   options.etu.development.php.enable = lib.mkEnableOption "Enable development php settings";
 
   config = lib.mkIf config.etu.development.enable {
+    # Enable persistence for composer files.
+    etu.base.zfs.user.directories = [
+      ".config/composer"
+    ];
+
     # Install php utils using home manager.
     etu.user.extraUserPackages = [
       # PHP utils
