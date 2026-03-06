3f9a43894d0fea6574f0de27a50f2518d1f5988e
Elis Hirwing <elis@hirwing.se>
2026-03-06 22:09:43 +0100

    fix(modules/flatpak): Always return true from the flatpak remote add

--- modules/nixos/graphical/flatpak/default.nix
+++ modules/nixos/graphical/flatpak/default.nix
@@ -61,7 +61,7 @@
       wantedBy = [ "multi-user.target" ];
       path = [ pkgs.flatpak ];
       script = ''
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo || true
       '';
     };
   };
