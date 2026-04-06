fef45299fec11951aa4d01444337201e76db8b58
Elis Hirwing <elis@hirwing.se>
2026-04-06 10:41:54 +0200

    fix(modules/emacs): Fix typescript-language-server nixpkgs path name

--- modules/home/emacs/default.nix
+++ modules/home/emacs/default.nix
@@ -46,7 +46,7 @@ let
     pkgs.bash-language-server # Bash language server
     pkgs.dockerfile-language-server # Docker language server
     pkgs.intelephense # PHP language server
-    pkgs.nodePackages.typescript-language-server # JS/TS language server
+    pkgs.typescript-language-server # JS/TS language server
     pkgs.vscode-langservers-extracted # CSS/LESS/SASS language server
     pkgs.nodejs # For copilot.el
 
