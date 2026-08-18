3d89fcf85c8f3a8b50aaba905ad0e0285c966856
Elis Hirwing <elis@hirwing.se>
2026-08-18 23:26:33 +0200

    fix(base/nix): Remove invalid mkIf wrapping on allowUnfreePredicate
    lib.mkIf only works on mergeable values like attrsets and lists, not
    on plain functions, so it had no effect here.

--- modules/nixos/base/nix/default.nix
+++ modules/nixos/base/nix/default.nix
@@ -19,14 +19,12 @@
   config = lib.mkIf config.etu.base.nix.enable {
     # If we allow certain unfree packages, enable the nix option to do so.
     nixpkgs.config.allowUnfreePredicate =
-      (lib.mkIf ((builtins.length config.etu.base.nix.allowUnfree) > 0))
-        (pkg: builtins.elem (lib.getName pkg) config.etu.base.nix.allowUnfree);
+      pkg: builtins.elem (lib.getName pkg) config.etu.base.nix.allowUnfree;
 
     # If we allow certain unfree packages on a home level, enable the nix option to do so.
     home-manager.users.${config.etu.user.username} = lib.mkIf config.etu.user.enable {
       nixpkgs.config.allowUnfreePredicate =
-        (lib.mkIf ((builtins.length config.etu.base.nix.allowUnfreeHome) > 0))
-          (pkg: builtins.elem (lib.getName pkg) config.etu.base.nix.allowUnfreeHome);
+        pkg: builtins.elem (lib.getName pkg) config.etu.base.nix.allowUnfreeHome;
     };
 
     # Extra binary caches
